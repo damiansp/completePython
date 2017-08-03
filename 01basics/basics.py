8f817b3dd4377d6a5aa08c42f101caeb2de6f21f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-08-03 02:18:12 -0400

    init

---
+++ 01basics/basics.py
@@ -0,0 +1,14 @@
+import cmath # complex math
+
+# Getting Input from User
+meaning = input('The meaning of life: ')
+print('%s, is it?' %meaning)
+
+x = int(input('x: '))
+y = int(input('y: '))
+print('xy = %d' %(x * y))
+
+
+# cmath and Complex Numbers
+print(cmath.sqrt(-1))
+print((1 + 3j) * (9 + 4j))
