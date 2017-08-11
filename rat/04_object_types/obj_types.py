313ba8e98b50fc4226d6f1ff408b70f7a43f9e38
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-08-11 10:44:00 -0700

    added list stuff

--- rat/04_object_types/obj_types.py
+++ rat/04_object_types/obj_types.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/envA python3
 import math
 import random
+import re
 
 print(math.sqrt(math.pi))
 print(random.choice([1, 2, 3, 5, 8]))
 
-# Strings
+
+
+# Strings------------------------------------------------------------------
 B = bytearray(b'spam') # byte/list hybrid
 B.extend(b'eggs')
 print('B:', B)
@@ -30,4 +33,60 @@ print('fancy formatting: {:,.2f}'.format(23434294872394.2384729837))
 print('more format options:\n%+.2f | %+05d' %(3.141596, -42))
 
 # Getting Help
+print(dir(s))
+print(help(s.replace))
+
+# Other ways to code strings
+print('ASCII binary value of "\\n":', ord('\n'))
+print('a\\x00B\\x00C:', 'a\x00B\x00C')
+
+# Unicode Strings
+print('Sp\xc4m') # \xc4 = A with umlaut
+print(b'a\x01c') # byte-based string (prints literally)
+print('a\x01c')  # Normal byte based string in 2; a^Ac in 3 (\x01 is byte)
+print(u'sp\u00c4m') # \u00c4 is also A with umlaut
+
+print('Spam')
+print('"Spam".encode("utf-8")', 'Spam'.encode('utf-8'))
+print('"Spam".encode("utf-16")', 'Spam'.encode('utf-16'))
+
+# Pattern Matching
+match = re.match('Hello[ \t]*(.*)world', 'Hello       Python world')
+print(match.group(1))
+match = re.match('[/:](.*)[/:](.*)[/:](.*)', '/usr/home:damiansp')
+print(match.groups())
+print(re.split('[/:]', '/usr/home/fancy/pants'))
+
+
+
+# Lists--------------------------------------------------------------------
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a + b)
+print(a * 3)
+print(a.pop(1))
+print(a)
+c = ['d', 'a', 'm', 'i', 'a', 'n']
+c.sort()
+print(c)
+c.reverse()
+print(c)
+
+# Generators
+G = (letter for letter in c)
+print(next(G))
+print(next(G))
+
+M = [[-3, -2, -1],
+     [ 0,  1,  2],
+     [ 3,  4,  5]]
+
+# Map
+print(list(map(sum, M)))
+
+print({sum(row) for row in M}) # Set
+print({i: sum(M[i]) for i in range(3)})  # Dict
+
+
+
 
