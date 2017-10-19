529812a18383cc7652d79a63b0b1bb13bab4165e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-10-19 00:16:00 -0400

    added template

---
+++ graphics/I/tkinter_template.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.title('')
+
+W, H = 700, 500
+canvas = Canvas(root, width=W, height=H, background='white')
+
+canvas.grid(row=0, column=1)
+
+
+
+
+
+root.mainloop()
