4c42ef141ec5671cdb2392812054be2a6102e6ab
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-10-30 20:39:02 -0400

    changed centers

--- graphics/II_animation/03_rotation/rotate_line.py
+++ graphics/II_animation/03_rotation/rotate_line.py
@@ -5,12 +5,13 @@ from tkinter import *
 sys.path.append('../../')
 from util import (make_discrete_color_series, Point, random_color, rotate)
 
-W, H = 700, 500
+W, H = 600, 750
 CENTER = Point(W/2, H/2)
 CYCLE_IN_MS = 50
-DEG_INCR = 1
-N_STEPS = 300
-N_LINES = 7
+DEG_INCR1 = random.uniform(-1, 1)
+DEG_INCR2 = random.uniform(-2, 2)
+N_STEPS = 1000
+N_LINES = 3
 
 
 def main():
@@ -28,8 +29,10 @@ def main():
 
     for i in range(N_STEPS):
         for j in range(len(lines)):
-            lines[j] = rotate(lines[j], DEG_INCR ** -1, centers[j])
-            lines[j] = rotate(lines[j], (j+1) * DEG_INCR, CENTER)
+            lines[j] = rotate(lines[j], DEG_INCR1, centers[j])
+            lines[j] = rotate(lines[j],
+                              (j+2) * DEG_INCR2,
+                              (lines[j][0] + lines[j][1]) * 0.5)
             canvas.create_line(
                 lines[j][0].as_list(), lines[j][1].as_list(), fill=colors[j][i])
         canvas.update()
