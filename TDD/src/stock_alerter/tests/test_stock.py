66be05acd181ebad226747d9c06a751b7fdd8d31
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-02-16 11:26:49 -0800

    restructures dirs; adds runTests.sh

--- TDD/src/stock_alerter/tests/test_stock.py
+++ TDD/src/stock_alerter/tests/test_stock.py
@@ -1,16 +1,10 @@
 import unittest
 
-class Stock:
-    def __init__(self, symbol):
-        self.symbol = symbol
-        self.price = None
-        
+from ..stock import Stock
+
 
 class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_should_be_None(self):
         stock = Stock('GOOG')
         self.assertIsNone(stock.price)
 
-
-if __name__ == '__main__':
-    unittest.main()
