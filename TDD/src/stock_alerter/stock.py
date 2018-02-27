a68f5690888223bce9b91fdd2c1a95cdbb5da51a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-02-27 11:30:40 -0800

    adds test_stock_update and test_negative_price_should_throw_ValueError

--- TDD/src/stock_alerter/stock.py
+++ TDD/src/stock_alerter/stock.py
@@ -2,3 +2,8 @@ class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
         self.price = None
+
+    def update(self, timestamp, price):
+        if price < 0:
+            raise ValueError('price cannot be negative')
+        self.price = price
--- TDD/src/stock_alerter/tests/test_stock.py
+++ TDD/src/stock_alerter/tests/test_stock.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from ..stock import Stock
 
@@ -7,4 +8,17 @@ class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_should_be_None(self):
         stock = Stock('GOOG')
         self.assertIsNone(stock.price)
+
+    def test_stock_update(self):
+        '''An update should set the price on the stock object. 
+        The timestamp uses the datetime module.'''
+        goog = Stock('GOOG')
+        goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, goog.price)
+
+    def test_negative_price_should_throw_ValueError(self):
+        goog = Stock('GOOG')
+        #with self.assertRaises(ValueError):
+        #    goog.update(datetime(2014, 2, 13), -1)
+        self.assertRaises(ValueError, goog.update, datetime(2014, 2, 13), -1)
 
