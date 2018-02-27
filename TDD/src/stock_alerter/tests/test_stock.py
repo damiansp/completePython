abff742258a554c1e1e1027b158aac580bfbca7f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-02-27 12:01:35 -0800

    adds test_stock_price_should_give_the_latest_price and adds setUp

--- TDD/src/stock_alerter/tests/test_stock.py
+++ TDD/src/stock_alerter/tests/test_stock.py
@@ -3,22 +3,42 @@ from datetime import datetime
 
 from ..stock import Stock
 
+# Common asserts:
+# assertAlmostEqual assertNotAlmostEqual
+# assertEqual       assertNotEqual
+# assertGreater     assertGreaterEqual
+# assertIn          assertNotIn
+# assertIs          assertIsNot
+# assertIsInstance  assertNotIsInstance
+# assertIsNone      assertIsNotNone
+# assertLess        assertLessEqual
+# assertRegex       assertNotRegex
+# assertTrue        assertFalse 
+# assertRaises
+# fail
 
 class StockTest(unittest.TestCase):
+    # override default setUp. this code run before every test.
+    def setUp(self):
+        self.goog = Stock('GOOG')
+        
     def test_price_of_a_new_stock_class_should_be_None(self):
-        stock = Stock('GOOG')
-        self.assertIsNone(stock.price)
+        self.assertIsNone(self.goog.price)
 
     def test_stock_update(self):
         '''An update should set the price on the stock object. 
         The timestamp uses the datetime module.'''
-        goog = Stock('GOOG')
-        goog.update(datetime(2014, 2, 12), price=10)
-        self.assertEqual(10, goog.price)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, self.goog.price)
 
     def test_negative_price_should_throw_ValueError(self):
-        goog = Stock('GOOG')
         #with self.assertRaises(ValueError):
-        #    goog.update(datetime(2014, 2, 13), -1)
-        self.assertRaises(ValueError, goog.update, datetime(2014, 2, 13), -1)
+        #    self.goog.update(datetime(2014, 2, 13), -1) # SAME AS:
+        self.assertRaises(
+            ValueError, self.goog.update, datetime(2014, 2, 13), -1)
 
+    def test_stock_price_should_give_the_latest_price(self):
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.goog.update(datetime(2014, 2, 13), price=8.4)
+        self.assertAlmostEqual(self.goog.price, 8.4, delta=0.0001)
+        #self.assertAlmostEqual(self.goog.price, 8.4, places=4) # alt option
