709a4bc5386696e1426ea7763919bf205d3f3a38
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-15 11:28:19 -0700

    adds calculator and calculator_test

--- testing/testing_python/02_unit_tests/calculator.py
+++ testing/testing_python/02_unit_tests/calculator.py
@@ -1,9 +1,12 @@
 class Calculator():
     def add(self, x, y):
-        return x + y
+        if type(x) == type(y) == int:
+            return x + y
+        else:
+            raise TypeError('Invalid type {} and {}'.format(type(x), type(y)))
 
 
 if __name__ == '__main__':
     calc = Calculator()
-    res = calc.add(2, 2)
+    res = calc.add(2, 3)
     print(res)
--- testing/testing_python/02_unit_tests/calculator_test.py
+++ testing/testing_python/02_unit_tests/calculator_test.py
@@ -10,6 +10,9 @@ class TestCalculator(unittest.TestCase):
     def test_add_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.add(2, 2))
 
+    def test_add_method_raises_typeError_if_not_ints(self):
+        self.assertRaises(TypeError, self.calc.add, 'hello', 'world')
+
 
 if __name__ == '__main__':
     unittest.main()
