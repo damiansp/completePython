678c97d92166973f19a74ed26410b2896826482c
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-02 21:24:08 -0400

    adds refactoring_strategy and inits *_funcitonal

--- fluent/06_first_class_function_design_patterns/refactoring_strategy_functional.py
+++ fluent/06_first_class_function_design_patterns/refactoring_strategy_functional.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from collections import namedtuple
 
 Customer = namedtuple('Customer', 'name fidelity')
@@ -35,43 +34,10 @@ class Order:
         return fmt.format(self.total(), self.due())
 
 
-
-class Promotion(ABC):
-    @abstractmethod
-    def discount(self, order):
-        '''Return discount as a positive dollar amount'''
+def fidelity_promo(order):
+    pass # TODO
 
 
-
-class FidelityPromo(Promotion):
-    '''5% discount for customers with 1000+ fidelity points'''
-    def discount(self, order):
-        return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
-
-
-
-class BulkItemPromo(Promotion):
-    '''10% discount for each LineItem with 20+ units'''
-    def discount(self, order):
-        discount = 0
-        for item in order.cart:
-            if item.quantity >= 20:
-                discount += item.total() * 0.1
-        return discount
-
-
-
-class LargeOrderPromo(Promotion):
-    '''7% discount for orders with 10+ distinct items'''
-    def discount(self, order):
-        distinct_items = {item.product for item in order.cart}
-        if len(distinct_items) >= 10:
-            return order.total() * 0.07
-        return 0
-
-
-
-john = Customer('John Doe', 0)
 ann = Customer('Ann Smith', 1100)
 cart = [LineItem('banana', 4, 0.5),
         LineItem('apple', 10, 1.5),
