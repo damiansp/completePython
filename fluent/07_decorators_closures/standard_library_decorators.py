a2d36ed9c1cdc1589ab410d3839a402dbb639248
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-23 15:15:47 -0400

    update stdlibdec, +param decor

--- fluent/07_decorators_closures/standard_library_decorators.py
+++ fluent/07_decorators_closures/standard_library_decorators.py
@@ -1,5 +1,8 @@
 import functools
 import html
+import numbers
+from collections import abc
+from functools import singledispatch
 
 from simple_decorator import better_clock as clock
 
@@ -19,10 +22,28 @@ def fibonacci(n):
     return fibonacci(n - 2) + fibonacci(n - 1)
 
 
+@singledispatch
 def htmlize(obj):
     content = html.escape(repr(obj))
     return '<pre>{}</pre>'.format(content)
 
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br\>\n')
+    return '<p>{0}</p>'.format(content)
+
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+
+
+@htmlize.register(tuple)
+@htmlize.register(abc.MutableSequence)
+def _(seq):
+    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+    return '<ul>\n<li>' + inner + '</li>\n</ul>'
 
 
 
