bc591bfee4d0608d116429cdc784589d65f62589
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-09 16:07:21 -0400

    fin weak ref; +tricks

--- fluent/08_object_references_mutability_recycling/weak_reference.py
+++ fluent/08_object_references_mutability_recycling/weak_reference.py
@@ -9,3 +9,25 @@ a_set = {2, 3, 4}
 print(wref()) # None
 print(wref() is None) # True
 
+
+
+class Cheese:
+    def __init__(self, kind):
+        self.kind = kind
+
+    def __repr__(self):
+        return 'Cheese (%r)' % self.kind
+
+
+stock = weakref.WeakValueDictionary()
+catalogue = [Cheese('Red Leicester'), Cheese('Tilsit'), Cheese('Brie'),
+             Cheese('Parmesan')]
+for cheese in catalogue:
+    stock[cheese.kind] = cheese
+print(sorted(stock.keys()))
+
+del catalogue
+print(sorted(stock.keys()))
+
+del cheese
+print(sorted(stock.keys()))
