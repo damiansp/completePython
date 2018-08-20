126d87677cdb106c47b6c1769a9efccd71a55029
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-08-20 01:08:09 -0400

    init diamond

---
+++ fluent/12_inheritance/diamond.py
@@ -0,0 +1,30 @@
+class A:
+    def ping(self):
+        print('ping:', self)
+
+class B(A):
+    def pong(self):
+        print('pong:', self)
+
+class C(A):
+    def pong(self):
+        print('PONG:', self)
+
+class D(B, C):
+    def ping(self):
+        super().ping() # same as A.ping(self)
+        print('post-ping:', self)
+
+    def pingpong(self):
+        self.ping()
+        super().ping()
+        self.pong()
+        super().pong()
+        C.pong(self)
+
+
+d = D()
+print(d.pong())
+print(C.pong(d))
+
+
