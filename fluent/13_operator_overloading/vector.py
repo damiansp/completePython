b027a4ab61c5fb9b26811e5b40f59c22df9d240d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-04 08:53:21 -0700

    vector + ne; copy in tombola

--- fluent/13_operator_overloading/vector.py
+++ fluent/13_operator_overloading/vector.py
@@ -35,6 +35,13 @@ class Vector:
         else:
             return NotImplemented
 
+    def __ne__(self, other):
+        eq_result = self == other
+        if eq_result is NotImplemented:
+            return NotImplemented
+        else:
+            return not eq_result
+        
     def __hash__(self):
         hashes = (hash(x) for x in self)
         return functools.reduce(operator.xor, hashes, 0)
