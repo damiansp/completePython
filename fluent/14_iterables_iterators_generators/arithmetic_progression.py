f21577fdaedaba1b7bfa08d5ea027aef95bb1988
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-30 22:23:11 -0400

    +arith progression

---
+++ fluent/14_iterables_iterators_generators/arithmetic_progression.py
@@ -0,0 +1,26 @@
+class ArithmeticProgression:
+    def __init__(self, begin, step, end=None):
+        self.begin = begin
+        self.step = step
+        self.end = end # None: arbitrarily long
+
+    def __iter__(self):
+        result = type(self.begin + self.step)(self.begin)
+        forever = self.end is None
+        index = 0
+        while forever or result < self.end:
+            yield result
+            index += 1
+            result = self.begin + self.step*index
+
+            
+# Functionally identical:
+def aritprog_gen(begin, step, end=None):
+    result = type(begin + step)(begin)
+    forever = end is None
+    index = 0
+    while forever or result < end:
+        yield result
+        index += 1
+        result = begin + step*index
+
