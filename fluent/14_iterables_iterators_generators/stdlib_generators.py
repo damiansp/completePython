da68ceccf66d765dd5554e0a004e8401c609c60f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-09 08:49:19 -0700

    stdlib++

--- fluent/14_iterables_iterators_generators/stdlib_generators.py
+++ fluent/14_iterables_iterators_generators/stdlib_generators.py
@@ -44,3 +44,27 @@ print(list(itertools.zip_longest('ABC', range(5))))
 # [(A 0) (B 1) (C 2) (None 3) (None 4)]
 print(list(itertools.zip_longest('ABC', range(5), fillvalue='?')))
 # [(A 0) (B 1) (C 2) (? 3) (? 4)]
+
+
+print(list(itertools.product('ABC', range(2)))) # [(A 0) (A 1) (B 0) (B 1) ...]
+suits = 'spades hearts diamonds clubs'.split()
+print(list(itertools.product('AK', suits)))
+print(list(itertools.product('ABC'))) # [(A,) (B,) (C,)]
+print(list(itertools.product('ABC', repeat=2))) # [(A A) (A B) (A C) (B A)...]
+print(list(itertools.product(range(2), repeat=3))) # [(0 0 0) (0 0 1) (0 1 0)..]
+rows = itertools.product('AB', range(2), repeat=2)
+for row in rows: print(row) # (A 0 A 0)\(A 0 A 1)\(A 0 B 0)...
+
+
+ct = itertools.count()
+print(next(ct)) # 0
+print(next(ct), next(ct), next(ct)) # (1, 2, 3)
+print(list(itertools.islice(itertools.count(1, 0.3), 3))) # [1 1.3 1.6]
+cy = itertools.cycle('ABC')
+print(next(cy)) # A
+print(list(itertools.islice(cy, 7))) # [B C A B C A B]
+rp = itertools.repeat(7)
+print(next(rp), next(rp)) # (7 7)
+print(list(itertools.repeat(8, 4))) # [8 8 8 8]
+print(list(map(operator.mul, range(11), itertools.repeat(5)))) # [0 5 10 15...]
+      
