ebfad72089bf0e89abc52eea84171ccf38832f9b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-18 16:07:45 -0500

    toolbox++

--- kivy_learn/03_widget_events_binding_actions/comiccreator/toolbox.py
+++ kivy_learn/03_widget_events_binding_actions/comiccreator/toolbox.py
@@ -11,13 +11,25 @@ kivy.require('1.9.0')
 
 class ToolButton(ToggleButton):
     def on_touch_down(self, touch):
-        ds = self.parent.drawing_space
-        if self.state == 'down' and ds.collide_point(touch.x, touch.y):
-            x, y = ds.to_widget(touch.x, touch.y)
-            self.draw(ds, x, y)
+        drawing_space = self.parent.drawing_space
+        if self.state == 'down' and drawing_space.collide_point(touch.x,
+                                                                touch.y):
+            x, y = drawing_space.to_widget(touch.x, touch.y)
+            self.draw(drawing_space, x, y)
             return True
-        retrun super(ToolButton, self).on_touch_down(touch)
+        return super(ToolButton, self).on_touch_down(touch)
 
-    def draw(self, ds, x, y):
-        pass
+    def draw(self, drawing_space, x, y):
+        stick_man = StickMan(width=48, height=48)
+        stick_man.center = (x, y)
+        drawing_space.add_widget(stick_man)
 
+
+        
+class ToolFigure(ToolButton):
+    def draw(self, drawing_space, x, y):
+        self.ix, self.iy = x, y
+        with drawing_space.canvas:
+            self.figure = self.create_figure(x, y, x + 1, y + 1)
+        drawing_space.bind(on_touch_move=self.update_figure)
+        drawing_space.bind(on_touch_up=self.end_figure)
