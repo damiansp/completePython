089eab34fc0c44d72632ec2365da4b7fc0a71fc6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-12-26 08:57:53 -0800

    Add taxi_sim

---
+++ fluent/16_coroutines/taxi_sim.py
@@ -0,0 +1,7 @@
+def taxi_process(ident, trips, start_time=0):
+    '''Yield to simulator issuing event at each state change'''
+    time = yield Event(start_time, ident, 'leave garage')
+    for i in range(trips):
+        time = yield Event(time, ident, 'pick up passenger')
+        time = yield Event(time, ident, 'drop off passenger')
+    yield Event(time, ident, 'going home')
