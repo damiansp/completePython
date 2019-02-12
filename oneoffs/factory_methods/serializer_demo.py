57fc2e30078b9df6fff0b312e13c2b55c3595849
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-02-12 08:55:21 -0800

    refactor

--- oneoffs/factory_methods/serializer_demo.py
+++ oneoffs/factory_methods/serializer_demo.py
@@ -64,4 +64,23 @@ class SongSerializer:
         serializer = self._get_serializer(format)
         return serializer(song)
 
-    def _get_serializer():
+    def _get_serializer(self, format):
+        if format == 'JSON':
+            return self._serialize_to_json
+        elif format == 'XML':
+            return self._serialize_to_xml
+        else:
+            raise ValueError(format)
+
+    def _serialize_to_json(self, song):
+        payload = {
+                'id': song.song_id, 'title': song.title, 'artist': song.artist}
+        return json.dumps(payload)
+
+    def _serialize_to_xml(self, song):
+        song_info = et.Element('song', attrib={'id': song.song_id})
+        title = et.SubElement(song_info, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_info, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_info, encoding='unicode')
