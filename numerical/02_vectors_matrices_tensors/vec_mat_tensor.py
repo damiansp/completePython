e8ee8a5164c68f2e4e36442f0198ca069c56d42a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-05 08:14:59 -0700

    add array init and conversion

--- numerical/02_vectors_matrices_tensors/vec_mat_tensor.py
+++ numerical/02_vectors_matrices_tensors/vec_mat_tensor.py
@@ -13,3 +13,15 @@ print(data.nbytes) # 48
 
 # dtypes:
 # int (int8, 16, 32, 64), uint (8-64), bool, float (16-128) complex (64-256)
+print(np.array([1, 2, 3], dtype=np.int))
+print(np.array([1, 2, 3], dtype=np.float))
+print(np.array([1, 2, 3], dtype=np.complex))
+
+data = np.array([1, 2, 3], dtype=np.float)
+print(data)
+print(data.astype(np.int))
+
+data = np.array([1, 2, 3], dtype=np.complex)
+print(data.real)
+print(data.imag)
+
