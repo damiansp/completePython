91ecb8318e9a3dfc4fb13890847cd17d9c2decf6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-08 07:26:34 -0700

    reorg dirs:

--- testing/pytest_learn/01_getting_started/tasks/test_three.py
+++ testing/pytest_learn/01_getting_started/tasks/test_three.py
@@ -1,6 +1,8 @@
 '''Test the Task data type.'''
 from collections import namedtuple
 
+import pytest
+
 
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
@@ -12,7 +14,7 @@ def test_defaults():
     t2 = Task(None, None, False, None)
     assert t1 == t2
 
-
+@pytest.mark.my_mark # allow tests to be run by mark name
 def test_member_access():
     '''Check .field functionality of namedtuple.'''
     t = Task('buy milk', 'Damian')
