be42779eda0d7aaad1cf584981868f4bcf7ec0a6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-08 07:08:32 -0700

    init pytest

---
+++ testing/pytest_learn/01_getting_started/test_one.py
@@ -0,0 +1,7 @@
+# Usage:
+#   pytest [-v] test_one.py
+
+def test_passing():
+    assert (1, 2, 3) == (1, 2, 3)
+
+
---
+++ testing/pytest_learn/01_getting_started/test_two.py
@@ -0,0 +1,4 @@
+# pytest [-v] test_two.py
+
+def test_failing():
+    assert (1, 2, 3) == (3, 2, 1)
