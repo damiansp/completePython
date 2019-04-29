afeb5da3bae5ef0a56e9c267fc6768f9472d9de3
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-29 08:40:06 -0700

    use matrix ops

--- numerical/02_vectors_matrices_tensors/03_matrix_and_vector_operations.py
+++ numerical/02_vectors_matrices_tensors/03_matrix_and_vector_operations.py
@@ -12,3 +12,22 @@ B = [[2, 3],
      [3, 4],
      [4, 5]]
 print(np.dot(A, B)) # [[11 14][20 26]]
+
+print(np.cross(a, b)) # -3 6 -3
+print(np.outer(a, b)) # [[0 0 0][3 4 5][6 8 10]]
+print(np.kron(a, b))  # kronecker prod
+
+# Express: A'  = BAB^1
+A = np.random.rand(3, 3)
+B = np.random.rand(3, 3)
+Ap = np.dot(B, np.dot(A, np.linalg.inv(B))) # or
+Ap = B.dot(A.dot(np.linalg.inv(B))) # To simplify...
+#A = np.matrix(A)
+#B = np.matrix(B)
+#Ap = B * A * B.I
+
+# Or using explicit casts
+A = np.asmatrix(A)
+B = np.asmatrix(B)
+Ap = B * A * B.I
+Ap = np.asarray(Ap)
