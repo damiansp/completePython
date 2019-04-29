31ad48ecc3593980d2c35ecae78520991b709306
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-29 00:56:49 -0400

    fin 05

--- advanced/05_local_and_recursive_functions.py
+++ advanced/05_local_and_recursive_functions.py
@@ -5,17 +5,25 @@ def factorial(x):
     return x * factorial(x = 1)
 
 
-def indented_list_sort(indented_list, indent=" "):
+def indented_list_sort(indented_list, indent=' '):
     KEY, ITEM, CHILDREN = range(3)
 
-    def add_entry(level, key, item, children):
+    def add_entry(key, item, children):
+        #if level == 0:
+        #    children.append((key, item, []))
+        #else:
+        #    add_entry(level - 1, key, item, children[-1][CHILDREN])
+        nonlocal level
         if level == 0:
             children.append((key, item, []))
         else:
-            add_entry(level - 1, key, item, children[-1][CHILDREN])
+            level -= 1
+            add_entry(key, item, children[-1][CHILDREN])
 
     def update_indented_list(entry):
-        pass
+        indented_list.append(entry[ITEM])
+        for subentry in sorted(entry[CHILDREN]):
+            update_indented_list(subentry)
 
     entries = []
     for item in indented_list:
@@ -25,8 +33,30 @@ def indented_list_sort(indented_list, indent=" "):
             i += len(indent)
             level += 1
         key = item.strip().lower()
-        add_entry(level, key, item, entries)
+        add_entry(key, item, entries)
     indented_list = []
     for entry in sorted(entries):
         update_indented_list(entry)
     return indented_list
+
+
+elements = ['Nonmetals',
+            '    H',
+            '    C',
+            '    N',
+            '    O',
+            'Inner Transitionals',
+            '    Lanthanides',
+            '        Ce',
+            '        Eu',
+            '    Actinides',
+            '        U',
+            '        Cm',
+            '        Pl',
+            'Alkali Metals',
+            '    Li',
+            '    Na',
+            '    K']
+elem_sorted = indented_list_sort(elements, indent='    ')
+for e in elem_sorted:
+    print(e)
