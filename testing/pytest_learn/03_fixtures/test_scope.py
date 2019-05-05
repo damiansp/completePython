a6f0fae11f3e3668ca4366e40ba4931aa4eea784
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-05 01:32:45 -0400

    set up db fixtures

--- testing/pytest_learn/03_fixtures/test_scope.py
+++ testing/pytest_learn/03_fixtures/test_scope.py
@@ -20,3 +20,23 @@ def sess_scope():
 @pytest.fixture(scope='class')
 def class_scope():
     '''A class scope fixture'''
+
+
+def test_1(sess_scope, mod_scope, func_scope):
+    '''Test using session, module, and function scope fixtures'''
+
+
+def test_2(sess_scope, mod_scope, func_scope):
+    '''Demo is more fun with multiple tests'''
+
+
+@pytest.mark.usefixtures('class_scope')
+class TestSomething():
+    '''Demo class scope fixtures'''
+
+    def test_3(self):
+        '''Test using a class scope fixture'''
+
+    def test_4(self):
+        '''Again, multiple tests are more fun'''
+        
