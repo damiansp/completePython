c53811ea18e00851f0def5702a74e204e67b4b42
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-06 14:56:08 -0400

    return form decorator

--- advanced/e1_decorators.py
+++ advanced/e1_decorators.py
@@ -1,3 +1,5 @@
+# Code here adapted from https://realpython.com/primer-on-python-decorators/
+
 def my_decorator(f):
     def wrapper():
         print(f'Before {f.__name__} is called')
@@ -38,5 +40,50 @@ say_yo()
 
 
 
+# Passing args
+
+@do_twice
+def greet(name):
+    print(f'Hello {name}')
+
+# greet('Señor Montelbaum') # TypeError: wrapper() takes 0 positional arguments
 
 
+def do_twice_new(f):
+    def wrapper(*args, **kwargs):
+        f(*args, **kwargs)
+        f(*args, **kwargs)
+    return wrapper
+
+
+@do_twice_new
+def say_hey():
+    print('Hey!')
+
+
+@do_twice_new
+def salute(title):
+    print(f'Hello, {title}')
+
+say_hey()
+salute('Doctor')
+
+
+
+# Returning vals
+
+def do_twice_and_return(f):
+    def wrapper(*args, **kwargs):
+        f(*args, **kwargs)
+        return f(*args, **kwargs)
+    return wrapper
+
+
+@do_twice_and_return
+def return_greeting(name):
+    print('Creating greeting')
+    return f'Hi {name}'
+
+
+hi_adam = return_greeting('Adam')
+print(hi_adam)
