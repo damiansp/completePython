9d1669ad779592d83f06329e37dad1523a714478
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-26 02:07:23 -0400

    basic widgets:

--- gui/tkinter/02_agrilabs/tkinter_widgets.py
+++ gui/tkinter/02_agrilabs/tkinter_widgets.py
@@ -1,4 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
-#my_entry = ttk.Entry(parent, textvariable=my_text_var)
+my_entry = ttk.Entry(parent, textvariable=my_text_var)
+my_spinbox = tk.Spinbox(
+    parent, from_=0.5, to=52., increment=0.01, textvariable=my_double_var)
+combo_box = ttk.Combobox(
+    parent, textvariable=my_str_var, values=['Opt 1', 'Opt 2', 'Opt 3'])
