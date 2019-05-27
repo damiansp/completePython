8b7a1d55910f2281335c40588520f19834b01baa
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-27 13:43:34 -0400

    init hello_opencv

--- comp_viz/open_cv4/hello_opencv/setup.py
+++ comp_viz/open_cv4/hello_opencv/setup.py
@@ -1,2 +1,12 @@
-'''setup.py'''
-py_modules = ['hello_opencv']
+from setuptools import setup
+
+setup(name='hello_opencv',
+      version='0.1.0',
+      py_modules = ['hello_opencv'],
+      license='MIT',
+      description='An example python opencv project',
+      long_description=open('README.md').read(),
+      isntall_requires=['numpy', 'opencv-python'],
+      url='https://github.com/albertofernandez',
+      author='Alberto Fernandez',
+      author_email='fernandezvillan.alberto@gmail.com')
