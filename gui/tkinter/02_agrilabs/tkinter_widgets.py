23413fad5d6b059e4ed699809ef39b4a79059b4d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-27 13:51:41 -0400

    more widget examples

--- gui/tkinter/02_agrilabs/tkinter_widgets.py
+++ gui/tkinter/02_agrilabs/tkinter_widgets.py
@@ -6,3 +6,12 @@ my_spinbox = tk.Spinbox(
     parent, from_=0.5, to=52., increment=0.01, textvariable=my_double_var)
 combo_box = ttk.Combobox(
     parent, textvariable=my_str_var, values=['Opt 1', 'Opt 2', 'Opt 3'])
+my_checkbutton = ttk.Checkbutton(
+    parent, text='Pick Me!', variable=my_boolean_var)
+
+# Creating a Text widget
+my_text = tk.Text(parent)
+my_text.insert('1.0', 'I love my text widget!')
+my_text.insert('1.2', 'REALLY ')
+my_text.get('1.0', tk.END)
+my_text.delete('end - 2 chars') # removes "!\n"
