3f5fe8d5d1f6ec498cbe751387f2eea930db2604
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-05-30 00:05:46 -0400

    add getter and setter to label

--- gui/tkinter/02_agrilabs/data_entry_app.py
+++ gui/tkinter/02_agrilabs/data_entry_app.py
@@ -11,6 +11,47 @@ class LabelInput(tk.Frame):
         input_args = input_args or {}
         label_args = label_args or {}
         self.variable = input_var
+        if input_class in (ttk.Checkbutton, ttk.Button, ttk.Radiobutton):
+            input_args['text'] = label
+            input_args['variable'] = input_var
+        else:
+            self.label = ttk.Label(self, text=label, **label_args)
+            self.label.grid(row=0, column=0, sticky=(tk.W + tk.E))
+            input_args['textvariable'] = input_var
+        self.input = input_class(self, **input_args)
+        self.input.grid(row=1, column=0, sticky=(tk.W + tk.E))
+        self.columnconfigure(0, weight=1)
+
+    def grid(self, sticky=(tk.E + tk.W), **kwargs):
+        super().grid(sticky=sticky, **kwargs)
+
+    def get(self):
+        try:
+            if self.variable:
+                return self.variable.get()
+            elif type(self.input) == tk.Text:
+                return self.input.get('1.0', tk.END)
+            else:
+                return self.input.get()
+        except (TypeError, tk.TclError): # if numeric fields are empty
+            return ''
+
+    def set(self, value, *args, **kwargs):
+        if type(self.variable) == tk.BooleanVar:
+            self.variable.set(bool(value))
+        elif self.variable:
+            self.variable.set(value, *args, **kwargs)
+        elif type(self.input) in (ttk.Checkbutton, ttk.Radiobutton):
+            if value:
+                self.input.select()
+            else:
+                self.input.deselect()
+        elif type(self.input) == tk.Text:
+            self.input.delete('1.0', tk.END)
+            self.input.insert('1.0', value)
+        else: # input mush be an Entry-type widget with no variable
+            self.input.delete(0, tk.END)
+            self.input.insert(0, value)
         
 
 class Application(tk.Tk):
