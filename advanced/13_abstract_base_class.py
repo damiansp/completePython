000125c13b213c997a204ab3763e6562b9154baa
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-06-17 07:28:47 -0700

    fin 13

--- advanced/13_abstract_base_class.py
+++ advanced/13_abstract_base_class.py
@@ -1,4 +1,7 @@
-class Appliacnce(metaclass=abc.ABCMeta):
+from collections import abc
+
+
+class Appliance(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __init__(self, model, price):
         self.__model = model
@@ -17,9 +20,36 @@ class Appliacnce(metaclass=abc.ABCMeta):
         return self.__model
 
 
-class Cookier(Appliance):
+class Cooker(Appliance):
     def __init__(self, model, price, fuel):
         super().__init__(model, price)
         self.fuel = fuel
         price = property(lambda self: super().price,
                          lambda self, price: super().set_price(price))
+
+
+class TextFilter(metaclass=abc.ABCMeta):
+    @abc.abstractproperty
+    def is_transformer(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def __call__(self):
+        raise NotImplementedError()
+
+
+class CharCounter(TextFilter):
+    @property
+    def is_transformer(self):
+        return False
+
+    def __call__(self, text, chars):
+        count = 0
+        for c in chars:
+            count += 1
+        return count
+
+    
+vowel_counter = CharCounter()
+print(vowel_counter('dog fish and cat fish', 'aeiou'))
+    
