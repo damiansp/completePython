b33b616d8665320bca32ae6dbfb4a3d37979852a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-06-21 09:21:33 -0700

    fin BGR

--- comp_viz/open_cv4/hello_opencv/BGR_RGB.py
+++ comp_viz/open_cv4/hello_opencv/BGR_RGB.py
@@ -6,7 +6,7 @@ IMG_DIR = 'images'
 
 
 img_openCV = cv2.imread(f'{IMG_DIR}/logo.png')
-b, g, r = cv2.split(img_openCV)
+b, g, r = cv2.split(img_openCV) # expensive
 img_matplotlib = cv2.merge([r, g, b])
 plt.subplot(121)
 plt.imshow(img_openCV)
@@ -15,3 +15,8 @@ plt.subplot(122)
 plt.imshow(img_matplotlib)
 plt.title('Channels Corrected')
 plt.show()
+
+B = img_OpenCV[:, :, 0]
+G = img_OpenCV[:, :, 1]
+R = img_OpenCV[:, :, 2]
+img_matplotlib = img_OpenCV[:, , ::-1]
