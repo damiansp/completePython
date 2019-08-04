c0f9f3aabb7c412ce715bf23055019d14698643a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-08-04 19:19:45 -0400

    add 3/constant and testing colors

---
+++ comp_viz/open_cv4/03_shapes/constant.py
@@ -0,0 +1,16 @@
+'''
+Common color triplets (BGR)
+'''
+
+BLACK      = (  0,   0,   0)
+RED        = (  0,   0, 255)
+GREEN      = (  0, 255,   0)
+YELLOW     = (  0, 255, 255)
+DARK_GREY  = ( 50,  50,  50)
+GREY       = (125, 125, 125)
+LIGHT_GREY = (220, 220, 220)
+BLUE       = (255,   0,   0)
+MAGENTA    = (255,   0, 255)
+CYAN       = (255, 255,   0)
+WHITE      = (255, 255, 255)
+
