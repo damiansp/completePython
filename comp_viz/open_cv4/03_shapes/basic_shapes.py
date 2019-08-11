a1ab9d1f070f0bc99bbbd317e997146532d639d8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-08-11 15:34:07 -0400

    add lines

--- comp_viz/open_cv4/03_shapes/basic_shapes.py
+++ comp_viz/open_cv4/03_shapes/basic_shapes.py
@@ -32,9 +32,19 @@ def show(img, title):
 
 
 def main():
-    image = np.zeros((400, 400, 3), dtype='uint8')
+    DIM = 400
+    image = np.zeros((DIM, DIM, 3), dtype='uint8')
     image[:] = colors['light_grey']
     show(image, 'just grey')
+
+    cv2.line(image, (0, 0), (DIM, DIM), colors['green'], 1) # 1=thickness
+    cv2.line(image, (0, DIM), (DIM, 0), colors['blue'], 2)
+    cv2.line(image, (DIM // 2, 0), (DIM // 2, DIM), colors['red'], 4)
+    cv2.line(image, (0, DIM // 2), (DIM, DIM // 2), colors['yellow'], 8)
+    show(image, 'lines')
+    
+    
+    
     
 
 if __name__ == '__main__':
