3f0cab45238bd8a9283e94a4e6cfe7cf9e5cacbb
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-10-21 07:44:28 -0700

    more transformations

--- comp_viz/open_cv4/05_geometric_transforms_of_images/transformations.py
+++ comp_viz/open_cv4/05_geometric_transforms_of_images/transformations.py
@@ -24,3 +24,21 @@ dst_image = cv2.warpAffine(image, M, (width, height))
 # (center), deg, scale
 M = cv2.getRotationMatrix2D((width / 2., height / 2.), 180, 1) 
 dst_image = cv2.warpAffine(image, M, (width, height))
+
+
+# Affine (e.g., Linear) Transformations
+pts_1 = np.float32([[135, 45], [385, 45], [135, 230]])
+pts_2 = np.float32([[135, 45], [385, 45], [150, 230]])
+M = cv2.getAffineTransform(pts_1, pts_2)
+dst_image = cv2.warpAffine(image_points, M, (width, height))
+
+
+# Perspective Transformation
+pts_1 = np.float32([[450, 65], [517, 65], [431, 164], [552, 164]])
+pts_2 = np.float32([[  0,  0], [300,  0], [  0, 300], [300, 300]])
+M = cv2.getPerspectiveTransform(pts_1, pts_2)
+dst_image = cv2.warpPerspective(image, M, (300, 300))
+
+
+# Cropping
+dst_image = image[80:200, 230:330]
