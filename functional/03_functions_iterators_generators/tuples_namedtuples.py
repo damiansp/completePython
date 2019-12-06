f39d4ae779c2d26fdff48986f6b8f90b9b8e5c9e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-12-06 08:33:47 -0800

    sep named tuples and gen expr into indiv files

--- functional/03_functions_iterators_generators/tuples_namedtuples.py
+++ functional/03_functions_iterators_generators/tuples_namedtuples.py
@@ -1,5 +1,3 @@
-import math
-
 from collections import namedtuple
 
 
@@ -13,44 +11,3 @@ print(red(mycolor))
 Color = namedtuple('Color', ('red', 'green', 'blue', 'name'))
 mycolor = Color(127, 222, 255, 'mycolor')
 print(mycolor.red)
-
-
-# Generator expressions
-def pfactorsl(x):
-    if x % 2 == 0:
-        yield 2
-        if x // 2 > 1: 
-            yield from pfactorsl(x // 2)
-        return
-    for i in range(3, int(math.sqrt(x) + 0.5) + 1, 2):
-        if x % i == 0:
-            yield i
-            if x // i > 1:
-                yield from pfactorsl(x // i)
-            return
-    yield x
-
-
-def pfactorsr(x):
-    def factor_n(x, n):
-        if n * n > x:
-            yield x
-            return
-        if x % n == 0:
-            yield n
-            if x // n > 1:
-                yield from facto_n(x // n, n)
-        else:
-            yield from factor_n(x, n + 2)
-    if x % 2 == 0:
-        yield 2
-        if x // 2 > 1:
-            yield from pfactorsr(x // 2)
-        return
-    yield from factor_n(x, 3)
-
-
-print(pfactorsl(1560))
-print(list(pfactorsl(1560)))
-                
-                                   
