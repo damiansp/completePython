008fd1e2339ddb27e356051110219f9894d660ba
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-12-18 07:42:10 -0800

    more generator examples

--- functional/03_functions_iterators_generators/generator_expressions.py
+++ functional/03_functions_iterators_generators/generator_expressions.py
@@ -1,4 +1,5 @@
 import csv
+from   collections import namedtuple
 import itertools
 import math
 
@@ -27,7 +28,7 @@ def pfactorsr(x):
         if x % n == 0:
             yield n
             if x // n > 1:
-                yield from facto_n(x // n, n)
+                yield from factor_n(x // n, n)
         else:
             yield from factor_n(x, n + 2)
     if x % 2 == 0:
@@ -52,7 +53,40 @@ def limits(iterable):
 # g_f_x = (g(y) for y in (f(x) for x in range(...)))
 
 def row_iter(source):
-    return csv.reader(source, delimiter='\t')
+    return csv.reader(source, delimiter=',')
 
 with open('Anscombe.txt') as source:
     print(list(row_iter(source)))
+
+
+def head_split_fixed(row_iter):
+    title = next(row_iter)
+    assert len(title) == 1 and title[0] == 'Anscombe\'s quartet'
+    heading = next(row_iter)
+    print(heading)
+    assert len(heading) == 4 and heading == ['I', 'II', 'III', 'IV']
+    columns = next(row_iter)
+    assert len(columns) == 8 and columns == ['x', 'y'] * 4
+    return row_iter
+
+with open('Anscombe.txt')as source:
+    print(list(head_split_fixed(row_iter(source))))
+
+
+# Lists, dicts, sets
+Pair = namedtuple('Pair', ('x', 'y'))
+
+
+def series(n, row_iter):
+    for row in row_iter:
+        yield Pair(*row[n * 2:n * 2 + 2])
+
+
+with open('Anscombe.txt') as source:
+    data = tuple(head_split_fixed(row_iter(source)))
+    s1 = tuple(series(0, data))
+    s2 = tuple(series(1, data))
+    s3 = tuple(series(2, data))
+    s4 = tuple(series(3, data))
+
+print(s4)
