3b232b2d26ea6045fe1f27056891cb8f0b0e52cc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-03 20:54:26 -0700

    Kwarg pos clarity++

--- effective/03_functions/25_kwarg_and_pos_clarity.py
+++ effective/03_functions/25_kwarg_and_pos_clarity.py
@@ -33,3 +33,36 @@ print(safer_div(1., 10**500, ignore_overflow=True)) # 0
 # But...
 print(safer_div(n=2, divisor=5)) # TypeError in some versions
 
+
+def saferer_div(n, div, /, *, ignore_overflow=False, ignore_zero_div=False):
+    try:
+        return n / divisor
+    except OverflowError:
+        return 0
+    except ZeroDivisionError:
+        if ignore_zero_div:
+            return float('inf')
+        else:
+            raise
+
+
+#print(saferer_div(n=2, div=5)) # TypeError
+
+
+def safest_div(
+        n, div, /, ndigits=10, *, ignore_overflow=False, ignore_zero_div=False):
+    try:
+        frac = n / div
+        return round(frac, ndigits)
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        raise
+
+print(safest_div(22, 7))
+print(safest_div(22, 7, 5))
+print(safest_div(22, 7, ndigits=4))
