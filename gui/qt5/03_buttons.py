8100a261020938810e77195265a43d83561501e1
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-08 21:30:49 -0700

    +buttons

---
+++ gui/qt5/03_buttons.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+import sys
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Buttons!'
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 240
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        button = QPushButton('Button', self)
+        button.setToolTip('Click me')
+        button.move(100, 70)
+        button.clicked.connect(self.on_click)
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        print('Right on!')
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
