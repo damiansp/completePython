35828e857438ee11ddff830aee264fd7ebac36c3
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-13 21:44:25 -0700

    fn def arg iter

--- effective/04_comprehension_generators/31_defensive_arg_iterating.py
+++ effective/04_comprehension_generators/31_defensive_arg_iterating.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterator
+
+
 def normalize(ns):
     total = sum(ns)
     res = []
@@ -38,10 +41,61 @@ assert sum(percentages) == 100.
 
 
 # But if ns is large this is memory wasteful...
-def normalize_funv(get_iter):
+def normalize_func(get_iter):
     total = sum(get_iter()) # New iterator
     res = []
-    for val in get_tier():  # New iterator again
+    for val in get_iter():  # New iterator again
         percent = 100 * val / total
         res.append(percent)
     return res
+
+
+path = 'my_numbers.txt'
+percentages = normalize_func(lambda: read_visits(path))
+print(percentages)
+assert sum(percentages) == 100.
+
+
+class ReadVisits:
+    def __init__(self, path):
+        self.path = path
+
+    def __iter__(self):
+        with open(self.path) as f:
+            for line in f:
+                yield int(line)
+
+visits = ReadVisits(path)
+perc = normalize(visits)
+print(perc)
+assert sum(perc) == 100.
+
+
+def normalize_defensive(ns):
+    if iter(ns) is ns: # ns is an iterator -- no good!
+        raise TypeError('Must supply a container')
+    total = sum(ns)
+    res = []
+    for val in ns:
+        perc = 100 * val / total
+        res.append(perc)
+    return res
+
+
+# Or, using collections
+def normalize_final(ns):
+    if isinstance(ns, Iterator):
+        raise TypeError('Must supply a container')
+    total = sum(ns)
+    res = []
+    for val in ns:
+        perc = 100 * val / total
+        res.append(perc)
+    return res
+
+perc = normalize_final(visits)
+assert sum(perc) == 100.
+
+visits = [15, 35, 80]
+it = iter(visits)
+normalize_final(it) # throws TypeError
