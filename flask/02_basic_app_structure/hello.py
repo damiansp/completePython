3fc452f2d0fd094f991ab24b1fe8357ad8aa5373
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-06 16:21:55 -0700

    init basic app structure

--- flask/02_basic_app_structure/hello.py
+++ flask/02_basic_app_structure/hello.py
@@ -1,5 +1,7 @@
+from flask import abort
 from flask import Flask
 from flask import make_response
+from flask import redirect
 
 
 app = Flask(__name__)
@@ -20,3 +22,17 @@ def set_cookie():
     resp = make_response('<h1>This page sets a cookie!</h1>')
     resp.set_cookie('answer', '42')
     return resp
+
+
+@app.route('/redirect')
+def redirect():
+    return redirect('http://www.example.com')
+
+
+@app.route('/login/<init>')
+def validate_user(init):
+    known = {'d': 'damian', 'a': 'abner'}
+    user = known.get(init, None)
+    if user is None:
+        abort(404)
+    return f'<h1>Hello, {user}</h1>'
