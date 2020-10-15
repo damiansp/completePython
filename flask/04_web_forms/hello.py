92b48aa770bf62c433d0293e85efc54188d5f5e8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-15 11:34:25 -0700

    init web forms

---
+++ flask/04_web_forms/hello.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtf_forms import StringField, SubmitField
+from wtf_forms.validators import DataRequired
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
