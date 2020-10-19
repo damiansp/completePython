1a4126a38a16e8a210bd9195bbf778ab5568dba9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-19 07:44:01 -0700

    copy in templates from 3, fix validator

--- flask/04_web_forms/hello.py
+++ flask/04_web_forms/hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abcdefg' # obviously don't do this....
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -17,16 +18,20 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+    def validate_on_submit(self):
+        return self.name.data in ['Damian', 'damian']
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template(
-        'index.html', form=form, name=name, current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
