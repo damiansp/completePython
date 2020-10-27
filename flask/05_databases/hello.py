80700ec133134c73f252800fc6814bb852ad9229
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-27 07:00:21 -0700

    add table cross refs

--- flask/05_databases/hello.py
+++ flask/05_databases/hello.py
@@ -26,7 +26,8 @@ moment = Moment(app)
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name = dbColumn(db.String(64), unique=True)
+    name = db.Column(db.String(64), unique=True)
+    users =db.relationship('User', backref='role')
 
     def __repr__(self):
         return f'<Role {self.name:r}>'
@@ -36,6 +37,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'<User {self.username:r}>'
