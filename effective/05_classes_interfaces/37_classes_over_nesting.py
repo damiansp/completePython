77bedd3800e27b1cd4e06248c7630f4dc04f9ce2
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-09 14:28:43 -0800

    init classes

---
+++ effective/05_classes_interfaces/37_classes_over_nesting.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+
+
+class SimpleGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = []
+
+    def report_grade(self, name, score):
+        self._grades[name].append(score)
+
+    def get_average_grade(self, name):
+        grades = self._grades[name]
+        return sum(grades) / len(grades)
+
+
+book = SimpleGradebook()
+book.add_student('Isaac Newton')
+book.report_grade('Isaac Newton', 90)
+book.report_grade('Isaac Newton', 95)
+book.report_grade('Isaac Newton', 87)
+print(book.get_average_grade('Isaac Newton'))
+
+
+class BySubjectGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def get_average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'PE', 90)
+book.report_grade('Albert Einstein', 'PE', 89)
+print(book.get_average_grade('Albert Einstein'))
+
+
+class WeightedGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append((score, weight))
+        
