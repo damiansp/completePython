85788ddf3d222090f8303c160450fd357954e9b0
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-10 13:22:05 -0800

    flesh out weightedGradebook

--- effective/05_classes_interfaces/37_classes_over_nesting.py
+++ effective/05_classes_interfaces/37_classes_over_nesting.py
@@ -66,3 +66,29 @@ class WeightedGradebook:
         grade_list = by_subject[subject]
         grade_list.append((score, weight))
         
+
+    # now overly complicated and difficult to read
+    def get_average_grade(self, name):
+        by_subject = self._grades[name]
+        score_sum, score_count = 0, 0
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+            score_sum += subject_avg / total_weight
+            score_count += 1
+        return score_sum / score_count
+
+# Interface also more complicated
+book = WeightedGradebook()
+book.add_student('Al')
+book.report_grade('Al', 'Math', 75, 0.05)
+book.report_grade('Al', 'Math', 65, 0.15)
+book.report_grade('Al', 'Math', 70, 0.80)
+book.report_grade('Al', 'PE', 100, 0.4)
+book.report_grade('Al', 'PE', 85, 0.6)
+print(book.get_average_grade('Al'))
+
+
+
