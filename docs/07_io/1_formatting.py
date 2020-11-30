d11480cc0584d3d04a020203b4c178ecaf960247
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-30 08:50:54 -0800

    +io docs

---
+++ docs/07_io/1_formatting.py
@@ -0,0 +1,8 @@
+# 3. Manual formatting
+for x in range(1, 11):
+    print(repr(x).rjust(2), repr(x * x).rjust(3), end=' ')
+    print(repr(x * x * x).rjust(4))
+
+print('12'.zfill(5))
+print('-3.14'.zfill(7))
+print('3.1415926'.zfill(5))
