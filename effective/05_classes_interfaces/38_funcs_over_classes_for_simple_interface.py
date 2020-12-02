88a8d65a9c4834bbe3c32c799675c69e2da25771
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-12-02 10:08:00 -0800

    fin 38 funcs over classes

--- effective/05_classes_interfaces/38_funcs_over_classes_for_simple_interface.py
+++ effective/05_classes_interfaces/38_funcs_over_classes_for_simple_interface.py
@@ -38,3 +38,35 @@ print('Res:', dict(res))
 print('Count:', count)
 
 
+class CountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def missing(self):
+        self.added += 1
+        return 0
+
+counter = CountMissing()
+res = defaultdict(counter.missing, current)
+for key, amt in increments:
+    res[key] += amt
+print(counter.added) # 2
+
+
+class BetterCountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def __call__(self):
+        self.added += 1
+        return 0
+
+counter = BetterCountMissing()
+assert counter() == 0
+assert callable(counter)
+
+counter = BetterCountMissing()
+res = defaultdict(counter, current)
+for k, amt in increments:
+    res[k] += amt
+print(counter.added) # 2
