006229771ec73e4d48d113dd80eea55f0f60cd14
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-12-08 08:43:54 -0800

    1st look++

--- docs/09_classes/03_first_look.py
+++ docs/09_classes/03_first_look.py
@@ -20,3 +20,51 @@ class Complex:
 
 x = Complex(3., -4.5)
 print(x)
+
+
+
+# 5. Class and Instance Variables
+class Dog:
+    kind = 'canine' # class var, shared by all instances
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+
+    def learn_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.learn_trick('shake')
+d.learn_trick('sit')
+print(d.name, d.kind, d.tricks) # Fido canine [shake, sit]
+print(e.name, e.kind) # Buddy canine
+
+
+
+# 4. Random Remarks
+# instance attributes override class attributes
+class Warehouse:
+    purpose = 'storage'
+    region = 'west'
+
+w1 = Warehouse()
+print(w1.purpose, w1.region) # storage west
+w2 = Warehouse()
+w2.region = 'east'
+print(w2.purpose, w2.region) # storage east
+
+
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def add2(self, x):
+        self.add(x)
+        self.add(x)
+
+
