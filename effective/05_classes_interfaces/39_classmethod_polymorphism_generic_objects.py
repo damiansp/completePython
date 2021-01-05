137ddf1389688e51835f146a1c80323aae76cbb9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-01-05 09:12:42 -0800

    Fin 39 classmethod polymorph

--- effective/05_classes_interfaces/39_classmethod_polymorphism_generic_objects.py
+++ effective/05_classes_interfaces/39_classmethod_polymorphism_generic_objects.py
@@ -92,3 +92,57 @@ class GenericInputData:
     @classmethod
     def generate_inputs(cls, config):
         raise NotImplementedError
+
+
+class PathInputData(GenericInputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+            
+class GenericWorker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reuduce(self, other):
+        raise NotImplementedError
+
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+
+
+class LineCountWorker(GenericWorker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+
+config = {'data_dir': tmpdir}
+res = mapreduce(LineCountWorker, PathInputData, config)
+print(f'There are {res} lines')
