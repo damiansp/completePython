0a4eee6f78d90377a5b7d19f9d8c2e16428ce00a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-01-11 20:41:41 -0800

    reinit django dir; ignore secure files

--- django/01_blog_app/blog_app/blog_app/settings.py
+++ django/01_blog_app/blog_app/blog_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2cl-+_4x%ggiuw0e*&7o8amwp08$iiphpd$4ozg02$u--q)3xh'
+SECRET_KEY = '051*!&+p0dp*i=!(5uk9r5tlkw5a2rbm5-4jujw27(wsg@l!+e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
