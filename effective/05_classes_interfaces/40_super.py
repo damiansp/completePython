3cfa4e9306d00227961dd0af7de1558f3b0e80db
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-01-20 07:34:56 -0800

    fin 40 super

--- effective/05_classes_interfaces/40_super.py
+++ effective/05_classes_interfaces/40_super.py
@@ -60,3 +60,60 @@ class ThisWay(TimesSeven, PlusNine):
         
 foo = ThisWay(5)
 print(f'Expected 5*7 + 9 = 44, but got: {foo.val}') # 14
+
+
+class TimesSevenCorrect(MyBaseClass):
+    def __init__(self, val):
+        super().__init__(val)
+        self.val *= 7
+
+
+class PlusNineCorrect(MyBaseClass):
+    def __init__(self, val):
+        super().__init__(val)
+        self.val += 9
+
+
+class GoodWay(TimesSevenCorrect, PlusNineCorrect):
+    def __init__(self, val):
+        super().__init__(val)
+
+
+class GoodWay2(PlusNineCorrect, TimesSevenCorrect):
+    def __init__(self, val):
+        super().__init__(val)
+
+
+foo = GoodWay(5)
+print('Expected 7(5 + 9) = 98; got:', foo.val)
+
+bar = GoodWay2(5) # outer op first
+print('Expected 7*5 + 9 = 44; got:', bar.val)
+print('\n'.join(repr(cls) for cls in GoodWay.mro())) # method resolution order
+print('\n'.join(repr(cls) for cls in GoodWay2.mro()))
+
+
+class ExplicitTrisect(MyBaseClass):
+    def __init__(self, val):
+        super(ExplicitTrisect, self).__init__(val) # = super().__init__(val)
+        self.val /= 3
+
+        
+class AutomaticTrisect(MyBaseClass):
+    def __init__(self, val):
+        super(__class__, self).__init__(val)
+        self.val /= 3
+
+
+# Prefer this form for simplicity
+class ImplicitTrisect(MyBaseClass):
+    def __init__(self, val):
+        super().__init__(val)
+        self.val /= 3
+
+assert ExplicitTrisect(9).val \
+    == AutomaticTrisect(9).val \
+    == ImplicitTrisect(9).val \
+    == 3
+
+
