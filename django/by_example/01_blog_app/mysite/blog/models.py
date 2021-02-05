b30a2f7e0c214ef6e6c5251ecbee2a5dee3da9b6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-02-05 08:47:56 -0800

    models++

--- django/by_example/01_blog_app/mysite/blog/models.py
+++ django/by_example/01_blog_app/mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+    
 class Post(models.Model):
     STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
     title = models.CharField(max_length=250)
@@ -15,6 +20,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
