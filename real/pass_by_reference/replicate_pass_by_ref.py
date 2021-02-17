416422eb1c24d6622b1c232a08d71d4980261f0e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-02-17 08:25:03 -0800

    replicate++

--- real/pass_by_reference/replicate_pass_by_ref.py
+++ real/pass_by_reference/replicate_pass_by_ref.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+from types import SimpleNamespace
+
+
 def square():
     # Bad choice!
     global n
@@ -12,3 +16,40 @@ def sq(n):
     return n * n
 
 print(sq(4)) # 16
+x = 4
+x = sq(x)
+
+
+def greet(name, counter):
+    return f'Hi, {name}!', counter + 1
+
+counter = 0
+for person in ['Alice', 'Bob', 'Chuck']:
+    greeting, counter = greet(person, counter)
+    print(greeting)
+    print('Counter:', counter)
+
+
+ns = SimpleNamespace()
+def sqr(instance):
+    instance.n *= instance.n
+
+ns.n = 4
+sqr(ns)
+print(ns.n) # 16
+
+
+NS = namedtuple('NS', 'n')
+ns = NS(4)
+print(ns.n) # 4
+#sqr(ns)    # AttributeError: can't set attribute
+
+
+class NS:
+    n = 4
+
+ns = NS()
+print(ns.n) # 4
+sqr(ns)
+print(ns.n) # 16 (instance attribute)
+print(NS.n) # 4  (class attribute)
