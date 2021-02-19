e09dfa021d9db9d5ea63552019d81949dda91fec
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-02-19 08:00:26 -0800

    add pagination

--- django/by_example/01_blog_app/mysite/blog/views.py
+++ django/by_example/01_blog_app/mysite/blog/views.py
@@ -1,11 +1,25 @@
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
 
+POSTS_PER_PAGE = 3
+
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, POSTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -16,3 +30,10 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = POSTS_PER_PAGE
+    template_name = 'blog/post/list.html'
