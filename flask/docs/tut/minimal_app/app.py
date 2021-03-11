d7950f22f2c1a4dad8b47fb233516953047c8a24
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-03-11 08:18:52 -0800

    Add template

--- flask/docs/tut/minimal_app/app.py
+++ flask/docs/tut/minimal_app/app.py
@@ -1,7 +1,7 @@
 # To execute
 # > export FLASK_APP=app.py
 # > flask run 
-from flask import Flask
+from flask import Flask, render_template, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ def index():
     return 'Index Page'
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, Workd!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/login')
+def login():
+    return('The log-in')
 
 
 @app.route('/user/<username>')
@@ -30,3 +36,10 @@ def show_post(post_id):
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     return f'Subpath {escape(subpath)}'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('show_user_profile', username='Bob Dobolina'))
