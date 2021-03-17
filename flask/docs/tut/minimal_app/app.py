e35309c20ebe34140a2d3f50b9d19f920eca8693
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-03-17 09:09:54 -0700

    app++

--- flask/docs/tut/minimal_app/app.py
+++ flask/docs/tut/minimal_app/app.py
@@ -1,15 +1,39 @@
 # To execute
 # > export FLASK_APP=app.py
 # > flask run 
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask, make_response, redirect, render_template, request, url_for)
 from markupsafe import escape
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    # Get cookie
+    username = request.cookies.get('username', None)
+    # ...
+    # set cookie:
+    resp = make_response(render_template('...'))
+    resp.set_cookie('username', 'the username')
+    return resp
+
+
+@app.route('/oldpage')
+def redirect_example():
+    return redirct(url_for('newpage'))
+
+
+@app.route('/abort')
+def abort_example():
+    abort(401)
+    unreachable_call()
+    
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 @app.route('/hello/')
@@ -18,10 +42,25 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return('The log-in')
+    error = None
+    if request.method == 'POST':
+        if is_valid_login(request.form['username'], request.form['password']):
+            return log_user_in(request.form['username'])
+        error = 'Invalid username/password'
+    # GET or bad POST
+    return render_template('login.html', error=error) 
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        #f.save('/var/www/uploads/uploaded_file.txt') # or
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+    # ...
+    
 
 @app.route('/user/<username>')
 def show_user_profile(username):
