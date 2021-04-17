daea27f02ab6a531e2427b6902d34d96d197fac6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-04-16 22:59:39 -0700

    +react method

--- justpy/10_custom_components/react_method.py
+++ justpy/10_custom_components/react_method.py
@@ -1,30 +1,6 @@
 import justpy as jp
 
 
-html = '''
-<div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" 
-     role="alert">
-  <div class="flex">
-    <div class="py-1">
-      <svg class="fill-current h-6 w-6 text-teal-500 mr-4" 
-           xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
-        />
-      </svg>
-    </div>
-    <div>
-      <p class="font-bold">Our privacy policy has changed</p>
-      <p class="text-sm">Make sure you know how these changes affect you.</p>
-    </div>
-  </div>
-</div>
-'''
-
-d = jp.parse_html(html)
-for c in d.commands:
-    print(c)
-
-
 BL = 'font-bold text-lg'
 DIV = ('bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 '
        'py-3 shadow-md')
@@ -52,15 +28,35 @@ class MyAlert(jp.Div):
         c6 = jp.Div(a=c2)
         c7 = jp.P(classes=BL, a=c6, text=self.title_text)
         c8 = jp.P(classes='text-sm', a=c6, text=self.body_text)
+        self.title_p = c7
+        self.body_p = c8
+
+    def react(self, data):
+        self.title_p.text = self.title_text
+        self.body_p.text = self.body_text
+        self.text = ''
 
 
+greetings = [
+    'Bonjour', 'Hola', 'Zdrastvujte', 'Ni hao', 'Salve', 'Konnichi wa',
+    'Guten Tag', 'Ola', 'Anyeong Haseyo', 'Asalaam Alaikum', 'Goddag',
+    'Shikamoo', 'Goedendag', 'Giasas', 'Dzien Dobry', 'Selamat Siang',
+    'Namaste', 'Merhaba', 'Shalom', 'God Dag', 'Trashi Delek']
+
+
+def translate(self, msg):
+    self.title_text = 'Hello'
+
+    
 def alert_test():
     wp = jp.WebPage()
-    d = MyAlert(
-        a=wp, classes=MW, title_text='Hello!', body_text='How do you do?')
-    d.title_text = 'Shalom!' # why does this do nothing?
-                             # - attr need to be assigned at render time, not
-                             #   creation time.... see react_method.py
+    for greeting in greetings:
+        d = MyAlert(a=wp,
+                    classes='m-2 w-1/4',
+                    title_text='hello',
+                    body_text='How is everyone?')
+        d.on('click', translate)
+        d.title_text = greeting
     return wp
 
 
