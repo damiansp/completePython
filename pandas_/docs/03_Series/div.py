aa79cbf1f4269e9fda7f1b68418a41cf20916617
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-09-29 06:56:39 -0700

    div++

--- pandas_/docs/03_Series/div.py
+++ pandas_/docs/03_Series/div.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from printit import printit
 
-
+# NOTE: <div> aliasies <divide>
 a = pd.Series([1, 1, 1, np.nan], index=list('abcd'))
 printit(a, 'a')
 
@@ -12,3 +12,6 @@ printit(b, 'b')
 
 adb = a.divide(b, fill_value=0)
 printit(adb, 'adb')
+
+adb2 = a.div(b, fill_value=0)
+printit(adb2, 'adb2')
