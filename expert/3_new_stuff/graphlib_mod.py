e97cf193959d2b48480fa42e887e4c20cb55757d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-10-12 06:59:41 -0700

    +graphlib

--- expert/3_new_stuff/graphlib_mod.py
+++ expert/3_new_stuff/graphlib_mod.py
@@ -1,1 +1,10 @@
-#
+from graphlib import TopologicalSorter
+
+
+table_refs = {'customers': set(),
+              'accounts': {'customers'},
+              'products': set(),
+              'orders': {'accounts', 'customers'},
+              'order_products': {'orders', 'products'}}
+sorter = TopologicalSorter(table_refs)
+print(list(sorter.static_order()))
