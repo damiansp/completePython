33cbec4d9994f548dd16c142508f94277c66e7d0
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-11-30 09:15:51 -0800

    init functional; xarray idx++

--- expert/4_cf_other_langs/functional.py
+++ expert/4_cf_other_langs/functional.py
@@ -1,1 +1,21 @@
-#
+import math
+
+
+def get_circle_area(r):
+    return math.pi * r**2
+
+
+R = 42
+print(get_circle_area(R))
+print(get_circle_area)
+print(get_circle_area.__class__)
+print(get_circle_area.__name__)
+
+get_c_area = lambda r: math.pi * r**2
+print()
+print(get_c_area(R))
+print(get_c_area)
+print(get_c_area.__class__)
+print(get_c_area.__name__)
+
+
