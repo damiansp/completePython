320be323830e7b95e292b3e2975764c8567d42f9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-12-02 09:21:30 -0800

    functional++

--- expert/4_cf_other_langs/functional.py
+++ expert/4_cf_other_langs/functional.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from functools import reduce
 import math
 
 
@@ -19,3 +21,36 @@ print(get_c_area.__class__)
 print(get_c_area.__name__)
 
 
+@dataclass
+class Person:
+    age: int
+    weight: int
+    name: str
+
+
+al = Person(40, 190, 'Al')
+bob = Person(45, 170, 'Bob')
+cris = Person(47, 110, 'Cris')
+dan = Person(38, 120, 'Dan')
+people = [al, bob, cris, dan]
+print(sorted(people, key=lambda person: person.age))
+
+sq_map = map(lambda x: x**2, range(10))
+print(sq_map)
+print(list(sq_map))
+
+mapped = list(map(print, range(5), range(4), range(5)))
+print(mapped)
+
+evens = filter(lambda n: n % 2 == 0, range(10))
+odds = filter(lambda n: n % 2 == 1, range(10))
+print(list(evens))
+print(list(odds))
+
+animals = ['giraffe', 'snake', 'lion', 'squirrel']
+s_animals = filter(lambda animal: animal.startswith('s'), animals)
+print(list(s_animals))
+      
+print(reduce(lambda a, b: a + b, [2, 2]))
+print(reduce(lambda a, b: a + b, [2, 2, 4]))
+print(reduce(lambda a, b: a + b, range(100)))
