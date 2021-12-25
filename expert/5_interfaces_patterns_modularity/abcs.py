5f8e46692be33b8bdb868cce9a1009a44ce4e6d2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-12-24 21:55:23 -0800

    linmod intro++; fin abcs

--- expert/5_interfaces_patterns_modularity/abcs.py
+++ expert/5_interfaces_patterns_modularity/abcs.py
@@ -19,6 +19,13 @@ class ColliderABC(ABC):
     def bbox(self):
         pass
 
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is ColliderABC:
+            if any('bbox' in B.__dict__ for B in C.__mro__):
+                return True
+        return NotImplemented
+
 
 @dataclass
 class Square(ColliderABC):
@@ -35,7 +42,24 @@ class Circle(ColliderABC):
     pass
 
 
+@dataclass
+class Line:
+    p1: Point
+    p2: Point
+
+    @property
+    def bbox(self):
+        # bc Line implements <bbox> it is a valid subclass of ColliderABC, by
+        # virtue of the __subclasshook__ method
+        return Box(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
+
+
 def find_collisions(objs):
     for obj in objs:
         if not isinstance(item, ColliderABC):
             raise TypeError(f'{obj} is not a collider')
+
+
+line = Line(Point(0, 0), Point(100, 100))
+print(line.bbox)
+print(isinstance(line, ColliderABC)) # True
