1d4229ca55e7677c232a52d50a81c904e02332c9
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-01-29 19:17:56 -0800

    estimation++, +synchronous

---
+++ expert/06_concurrency/synchronous.py
@@ -0,0 +1,28 @@
+import time
+
+import requests
+
+
+SYMBOLS = ('USD', 'EUR', 'PLN', 'NOK', 'CZK')
+BASES = ('USD', 'EUR', 'PLN', 'NOK', 'CZK')
+
+
+def fetch_rates(base):
+    resp = requests.get(f'https://api.vatcomply.com/rates?base={base}')
+    resp.raise_for_status()
+    rates = resp.json()['rates']
+    rates[base] = 1.
+    rates_line = ', '.join([f'{rates[sym]:7.03} {sym}' for sym in SYMBOLS])
+    print(f'1 {base}: {rates_line}')
+    
+
+def main():
+    for base in BASES:
+        fetch_rates(base)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    elapsed = time.time() - start
+    print(f'\ntime elapsed: {elapsed:.2f} s')
