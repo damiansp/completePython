8552ae4969ee17a9f2ca53c6dc409ec1cff66685
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-02-20 18:32:57 -0800

    init user based sess

---
+++ plotly/tutorial/docs/5_sharing_data_between_callbacks/user_based_session_data.py
@@ -0,0 +1,52 @@
+import datetime
+import os
+import time
+import uuid
+
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+from flask_caching import Cache
+import pandas as pd
+
+
+styles = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+          'https://codepen.io/chriddyp/pen/brPBPO.css']
+app = Dash(__name__, external_stylesheets=styles)
+cache = Cache(
+    app.server,
+    config={
+        #'CACHE_TYPE': 'redis',
+        'CACHE_TYPE': 'filesystem',
+        'CACHE_DIR': 'cache-directory',
+        'CACHE_THRESHOLD': 10}) # max users on app at a single time
+
+
+def get_df(session_id):
+    @cache.memoize()
+    def query_and_serialize_data(session_id):
+        # expensive or user/sess-unique data processing here
+        # Here: simulate user/sess-unique data processing step by generating
+        # data dependent on time
+        now = datetime.datetime.now()
+        # Sim expensive process
+        time.sleep(3)
+        df = pd.DataFrame({
+            'time': [str(now - datetime.timedelta(seconds=s))
+                     for s in [15, 10, 5, 0]],
+            'values': ['a', 'b', 'a', 'c']})
+        return df.to_json()
+
+    return pd.read_json(query_and_serialize_data(session_id))
+
+
+def serve_layout():
+    session_id = str(uuid.uuid4())
+    return html.Div([
+        dcc.Store(data=session_id, id='session-id'),
+        html.Button('Get data', id='get-data-button'),
+        html.Div(id='output-1'),
+        html.Div(id='output-2')])
+
+
+app.layout = serve_layout
+                        
