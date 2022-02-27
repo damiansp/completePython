8921b063e305a631978b6a979d1d75656e7cb40c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-02-26 19:08:54 -0800

    setup++

--- expert/11_packaging_distributing/packaging_distributing_libraries/setup.py
+++ expert/11_packaging_distributing/packaging_distributing_libraries/setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup
 
 
-setup(name='mypackage')
+setup(
+    name='solrq',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audienc :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'],
+    version='0.1.1')
