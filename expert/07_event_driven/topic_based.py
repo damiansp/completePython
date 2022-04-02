5b5afe2a7ebd3f9f099d8b2dc9f0696ef6aca7b7
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-04-01 23:56:11 -0700

    class decor++, +topic based events

---
+++ expert/07_event_driven/topic_based.py
@@ -0,0 +1,24 @@
+import itertools
+from blinker import signal
+
+
+class SelfWatch:
+    _new_id = itertools.count(1)
+
+    def __init__(self):
+        self._id = next(self._new_id)
+        init_signal = signal('SelfWatch.init')
+        init_signal.send(self)
+        init_signal.connect(self.receiver)
+
+    def receiver(self, sender):
+        print(f'{self}: received event from {sender}')
+
+    def __str__(self):
+        return f'<{self.__class__.__name__}: {self._id}>'
+
+
+selfwatch1 = SelfWatch()
+selfwatch2 = SelfWatch()
+selfwatch3 = SelfWatch()
+selfwatch4 = SelfWatch()
--- serious/07_methods_decorators/class_decorators.py
+++ serious/07_methods_decorators/class_decorators.py
@@ -1,3 +1,5 @@
+import functools
+import inspect
 import uuid
 
 
@@ -52,10 +54,85 @@ print(fubar.__doc__) # Hammer shit
 print(fubar.__name__) # fubar
 
 
-@is_admin
+ppppppp@is_admin
 def fubar(username='anon'):
     '''Hizammer shizit'''
     pass
 
 print(fubar.__doc__) # None :(
 print(fubar.__name__) # wrapper :(
+
+
+WRAPPER_ASSIGNMENTS = (
+    '__module__', '__name__', '__qualname__', '__doc__', '__annotations__')
+WRAPPER_UPDATES = ('__dict__',)
+
+
+def update_wrapper(
+        wrapper, wrapped, assigned=WRAPPER_ASSIGNMENTS, updated=WRAPPER_UPDATES
+):
+    for attr in assignied:
+        try:
+            val = getattr(wrapped, attr)
+        except AttributeError:
+            pass
+        else:
+            setattr(wrapper, attr, val)
+    for attr in updated:
+        getattr(wrapper, attr).update(getttr(wrapped, attr, {}))
+    wrapper.__wrapped__ = wrapped
+    return wrapper
+
+
+def fubar(username='user'):
+    '''Hammer time!'''
+    pass
+
+fubar = functools.update_wrapper(is_admin, fubar)
+print(fubar.__name__)  # fubar
+print(fubar.__doc__)   # Hammer time!
+
+
+def check_is_admin(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if kwargs.get('username') != 'admin':
+            raise Exception('Access Denied')
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def Store:
+    @check_is_admin
+    def get_food(self, username, food):
+        '''Get food from storage'''
+        return self.storage.get(food)
+
+
+def check_is_admin(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        func_args = inspect.getcallargs(f, *args, **kwargs)
+        if func_arg.get('username') != 'admin':
+            raise Exception('Access Denied')
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@check_is_admin
+def get_food(username, variety):
+    return variety + ', nom nom nom'
+
+
+class Pizza:
+    def __init__(self, size):
+        self.size = size
+
+    def get_size(self):
+        return self.size
+
+
+print(Pizza.get_size)  # <function Pizza.get_size at 0x...>
+
