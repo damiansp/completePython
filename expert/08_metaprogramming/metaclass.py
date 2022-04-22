914c06a75285966a973c40b7f14c5a6ebb8ab529
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-04-21 22:09:47 -0700

    metaclass++

--- expert/08_metaprogramming/metaclass.py
+++ expert/08_metaprogramming/metaclass.py
@@ -1,3 +1,7 @@
+import inflection
+from typing import Any
+
+
 # General syntax
 def method(self):
     return 1
@@ -17,7 +21,7 @@ class ClassWithMetaclass(metaclass=type):
 
 
 # Common template
-class Metaclass(type):
+class MetaClass(type):
     def __new__(mcs, name, bases, namespace):
         return super().__new__(mcs, name, bases, namespace)
 
@@ -25,13 +29,76 @@ class Metaclass(type):
     def __prepare__(mcs, name, bases, **kwargs):
         return super().__prepare__(name, bases, **kwargs)
 
-    def __init__(cls, name, bases, naemespace, **kwargs):
+    def __init__(cls, name, bases, namespace, **kwargs):
         super().__init__(name, bases, namespace)
 
     def __call__(cls, *args, **kwargs):
         return super().__call__(*args, **kwargs)
 
 
-class Klass(metaclass=MetaClass, extra='value'):
-    pass
+#class Klass(metaclass=MetaClass, extra='value'):
+#    pass
 
+
+class RevealingMeta(type):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        print(mcs, 'METACLASS __new__ called')
+        return super().__new__(mcs, name, bases, namespace)
+
+    @classmethod
+    def __prepare__(mcs, name, bases, **kwargs):
+        print(mcs, 'METACLASS __prepare__ called')
+        return super().__prepare__(name, bases, **kwargs)
+
+    def __init__(cls, name, bases, namespace, **kwargs):
+        print(cls, 'METACLASS __init__ called')
+        super().__init__(name, bases, namespace)
+
+    def __call__(cls, *args, **kwargs):
+        print(cls, 'METACLASS __call__ called')
+        return super().__call__(*args, **kwargs)
+
+
+class RevealingClass(metaclass=RevealingMeta):
+    def __new__(cls):
+        print(cls, '__new__ called')
+        return super().__new__(cls)
+
+    def __init__(self):
+        print(self, '__init__ called')
+        super().__init__()
+
+
+rc = RevealingClass()
+
+
+# Example usage of __prepare__: allow camelCase function names to be used and
+# automatically converted to snake_case
+class FlexiCamelDict(dict):
+    def __setitem__(self, key: str, value: Any):
+        super().__setitem__(key, value)
+        super().__setitem__(inflection.underscore(key), value)
+
+
+class FlexiCamelMeta(type):
+    @classmethod
+    def __prepare__(mcs, name, bases):
+        return FlexiCamelDict()
+
+
+class User(metaclass=FlexiCamelMeta):
+    def __init__(self, firstName: str, lastName: str):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def getDisplayName(self):
+        return f'{self.firstName} {self.lastName}'
+
+    def greetUser(self):
+        return f'Hello, {self.getDisplayName()}!'
+
+
+user = User('Bob', 'Dobolina')
+print(user.get_display_name())  # !!
+print(user.greet_user())
+print(User.__dict__)
