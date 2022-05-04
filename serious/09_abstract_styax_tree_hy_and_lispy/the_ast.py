ae49918c92e1a379358e54a29e4b57ae1cb432a1
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-05-04 10:03:11 -0700

    ast++

--- serious/09_abstract_styax_tree_hy_and_lispy/the_ast.py
+++ serious/09_abstract_styax_tree_hy_and_lispy/the_ast.py
@@ -4,3 +4,28 @@ import ast
 print(ast.parse)
 print(ast.parse('x = 42'))
 print(ast.dump(ast.parse('x = 42')))
+
+
+print(compile(ast.parse('x = 42'), '<input>', 'exec'))
+eval(compile(ast.parse('x = 42'), '<input>', 'exec'))
+print(x)
+
+
+hello = ast.Str(s='Hello, World!', lineno=1, col_offset=1)
+print_name = ast.Name(id='print', ctx=ast.Load(), lineno=1, col_offset=1)
+print_call = ast.Call(
+    func=print_name,
+    ctx=ast.Load(),
+    args=[hello],
+    keywords=[],
+    lineno=1,
+    col_offset=1)
+module = ast.Module(
+    body=[ast.Expr(print_call, lineno=1, col_offset=1)],
+    lineno=1,
+    col_offset=1,
+    type_ignores=[])
+code = compile(module, '', 'exec')
+eval(code)
+
+
