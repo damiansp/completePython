28341c8b9d62e08109e25ae6a5f60bdd020674f5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-12 20:33:11 -0700

    fin fib.ptx w pure c call and gil release

--- expert/09_python_with_c_cpp/cython/fibonacci.pyx
+++ expert/09_python_with_c_cpp/cython/fibonacci.pyx
@@ -1,6 +1,11 @@
 '''Cython module for fibonacci sequence'''
+cdef long long fibonacci_cc(unisigned int n) nogil:
+    if n < 2:
+        return n
+    return fibonacci_cc(n - 1) + fibonacci_cc(n - 2)
+
+
 def fibonacci(unsigned int n):
     '''Return the nth Fibonacci number (computed recursively).'''
-    if n <= 1
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 1)
+    with nogil:
+        return fibonacci_cc(n)
