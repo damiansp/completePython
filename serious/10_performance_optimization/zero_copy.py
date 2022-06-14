d77e0f1a64159b15c1243e64dc3342ec6f9cbeaf
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-14 08:41:49 -0700

    serious: zero copy++

--- serious/10_performance_optimization/zero_copy.py
+++ serious/10_performance_optimization/zero_copy.py
@@ -1,5 +1,5 @@
 @profile
-def read_random():
+def read_random_poor():
     with open('./random.pkl', 'rb') as f:
         content = f.read(1024 * 10_000)
         to_do = content[1024:]
@@ -8,5 +8,16 @@ def read_random():
         f.write(to_do)
 
 
+@profile
+def read_random():
+    with open('./random.pkl', 'rb') as f:
+        content = f.read(1024 * 10_000)
+        to_do = memoryview(content)[1024:]
+    print(f'Content length: {len(content)}\nto do: {len(to_do)}')
+    with open('./rand_partial_copy.pkl', 'wb') as f:
+        f.write(to_do)
+        
+
 if __name__ == '__main__':
+    read_random_poor()
     read_random()
