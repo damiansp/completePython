ba96109d46c5108c754746a6b563101efb21470c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-15 08:47:35 -0700

    stdlib/init binary seqs

---
+++ stdlib/3.10/stdtypes/binary_sequences.py
@@ -0,0 +1,24 @@
+# Bytes, bytearrays, memoryviews
+
+
+# Bytes
+bytestr = b'any old ascii string here'
+zero_filled = bytes(10)
+from_hex = bytes.fromhex('2Ef0 F1f2  ')
+print(from_hex)
+to_hex = b'.\xf0\xf1\xf2'.hex()
+print(to_hex)
+
+
+# Bytearray (mutable counterpart to bytes)
+from_hex = bytearray.fromhex('2Ef0 f1F2  ')
+to_hex = bytearray(b'.\xf0\xf1\xf2').hex()
+print(from_hex)
+print(to_hex)
+
+
+# Ops on bytes/bytearray
+a = b'abc'
+b = a.replace(b'a', b'z')
+print(b)
+
