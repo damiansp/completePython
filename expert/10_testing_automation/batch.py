5ec6b4e4551a97f64d6ae4d7a647dd1ae1fd1bef
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-16 08:30:18 -0700

    expert/10 batch++

--- expert/10_testing_automation/batch.py
+++ expert/10_testing_automation/batch.py
@@ -1,5 +1,11 @@
+from itertools import islice
 from typing import Any, Iterable, List
 
 
 def batches(iterable: Iterable[Any], batch_size: int) -> Iterable[List[Any]]:
-    pass
+    iterator = iter(iterable)
+    while True:
+        batch = list(islice(iterator, batch_size))
+        if not batch:
+            return
+        yield batch
