4c189dd864305dc3f981fd8b476eb63a6608a07c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-21 13:46:48 -0700

    serious/13/fin attrs

--- serious/13_write_less_code_more/attrs.py
+++ serious/13_write_less_code_more/attrs.py
@@ -1,4 +1,47 @@
-class Car:
+import attr
+
+
+class OldCar:
     def __init__(self, color, speed=0):
         self.color = color
         self.speed = speed
+
+
+@attr.s
+class Car:
+    color = attr.ib(converter=str)
+    speed = attr.ib(default=0)
+
+    @speed.validator
+    def validate_speed(self, attribute, val):
+        if val < 0:
+            raise ValueError('Value cannot be negative')
+
+        
+c = Car('blue')
+print(c)
+
+try:
+    d = Car('red', -5)
+except ValueError as e:
+    print(e)
+
+
+@attr.s(frozen=True)
+class CoolCar:
+    color = attr.ib(converter=str)
+    speed = attr.ib(default=0)
+
+    @speed.validator
+    def validate_speed(self, attribute, val):
+        if val < 0:
+            raise ValueError('Value cannot be negative')
+
+
+cc = CoolCar('blue')
+try:
+    cc.color = 'red'
+except BaseException as e:
+    print('Cannot change color')
+    print(e)
+print(cc.color)
