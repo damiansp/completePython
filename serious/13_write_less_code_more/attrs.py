01676863c0b0f839f14c197b0c373a4e7786da58
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-21 08:56:39 -0700

    /serious/13/ fin ctxt mgr, init attrs

---
+++ serious/13_write_less_code_more/attrs.py
@@ -0,0 +1,4 @@
+class Car:
+    def __init__(self, color, speed=0):
+        self.color = color
+        self.speed = speed
--- serious/13_write_less_code_more/context_managers.py
+++ serious/13_write_less_code_more/context_managers.py
@@ -1,6 +1,59 @@
-class MyContext:
+import contextlib
+
+
+class MyBasicContext:
     def __enter__(self):
         pass
 
     def __exit__(self, exc_type, ecv_value, traceback):
         pass
+
+
+@contextlib.contextmanager
+def my_context():
+    print('Do something first')
+    yield
+    print('Do something last')
+
+
+with my_context():
+    print('Hello, World!')
+
+
+@contextlib.contextmanager
+def meaningful_context():
+    print('Do something first')
+    yield 42
+    print('Do something last')
+
+
+with meaningful_context() as meaning:
+    print(meaning)
+
+
+@contextlib.contextmanager
+def meaningful_context2():
+    print('Do something first')
+    try:
+        yield 42
+    finally:
+        print('Do something last')
+
+
+#with meaningful_context2() as meaning:
+#    print('Raise da roof!')
+#    raise ValueError('Raise it!')
+#    print(meaning)
+
+
+# multiple better combined... instead of:
+'''
+with open('f1', 'r') as infile:
+    with open('f2', 'w') as outfile:
+        outfile.write(infile.read())
+'''
+# do:
+'''
+with open('f1', 'r') as infile, open('f2', 'w') as outfile:
+    outfile.write(infile.read())
+'''
