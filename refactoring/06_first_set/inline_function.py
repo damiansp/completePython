89e4edefd7ca049af4f56d181a11492713151bcf
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2022-07-24 20:28:50 -0700

    refactor/inline func++

--- refactoring/06_first_set/inline_function.py
+++ refactoring/06_first_set/inline_function.py
@@ -9,3 +9,26 @@ def has_more_than_five_late_deliveries(driver):
 # ->
 def get_rating(driver):
     return 2 if driver.n_late_deliveries > 5 else 1
+
+
+
+def report_lines(customer):
+    lines = []
+    lines = gather_customer_data(lines, customer)
+    return lines
+
+
+def gather_customer_data(out, customer):
+    out.append(['name', customer.name])
+    out.append(['location', customer.location])
+    return out
+
+
+# ->
+def report_lines(customer):
+    lines = []
+    lines.append(['name', customer.name])
+    lines.append(['location', customer.location])
+    return lines
+
+
