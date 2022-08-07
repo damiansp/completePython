8a1464f496096ed9a5b68b88c9961c387520fb60
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-08-06 17:25:38 -0700

    fin nbody.py

--- cython/smith/04_n_body_sim/pure_python/nbody.py
+++ cython/smith/04_n_body_sim/pure_python/nbody.py
@@ -4,12 +4,17 @@ import sys
 from static import BODIES
 
 
+TIMESTEP = 0.01
+
+
 def main(n, bodies=BODIES, ref='sun'):
     system = list(bodies.values())
     pairs = combinations(system, 2)
-    offset_momentum(bodes[ref], system)
-    report_energy(bodies, pairs)
-
+    offset_momentum(bodies[ref], system)
+    report_energy(system, pairs)
+    advance(TIMESTEP, n, system, pairs)
+    report_energy(system, pairs)
+    
     
 def offset_momentum(ref, bodies):
     px = py = pz = 0.
@@ -24,7 +29,36 @@ def offset_momentum(ref, bodies):
 
 
 def report_energy(bodies, pairs):
-    
+    e = 0.
+    for ((p1, v1, m1), (p2, v2, m2)) in pairs:
+        dx, dy, dz = get_deltas(p1, p2)
+        e -= (m1 * m2) / ((dx*dx + dy*dy + dz*dz) ** 0.5)
+    for (r, [vx, vy, vz], m) in bodies:
+        e += m * (vx*vx + vy*vy + vz*vz) / 2.
+    print(f'{e:.9f}')
+
+
+def advance(dt, n, bodies, pairs):
+    for i in range(n):
+        for ((p1, v1, m1), (p2, v2, m2)) in pairs:
+            dx, dy, dz = get_deltas(p1, p2)
+            mag = dt * ((dx*dx + dy*dy + dz*dz) ** (-1.5))
+            b1m = m1 * mag
+            b2m = m2 * mag
+            v1[0] -= dx * b2m
+            v1[1] -= dy * b2m
+            v1[2] -= dz * b2m
+            v2[0] -= dx * b1m
+            v2[1] -= dy * b1m
+            v2[2] -= dz * b1m
+        for (r, [vx, vy, vz], m) in bodies:
+            r[0] += dt * vx
+            r[1] += dt * vy
+            r[2] += dt * vz
+
+            
+def get_deltas(p1, p2):
+    return [a - b for a, b in zip(p1, p2)]
 
 
 if __name__ == '__main__':
