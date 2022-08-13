3e5a3d66f35c36e149d065689d727fba7b7b07a2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-08-13 13:03:56 -0700

    cython/nbody/ cython v++

--- cython/smith/04_n_body_sim/cython/nbody.pyx
+++ cython/smith/04_n_body_sim/cython/nbody.pyx
@@ -41,5 +41,43 @@ def report_energy(bodies):
     print(f'{e:.9f}')
 
 
-def advance():
+def advance(double dt, int n, bodies):
+    cdef:
+        int i, ii, jj
+        double dx, dy, dz, mag, b1m, b2m, ds
+        body_t *body1
+        body_t *body2
+        body_t cbodies[N_BODIES]
+    make_cbodies(bodies, cbodies, N_BODIES)
+    for i in range(n):
+        for ii in range(N_BODIES -1):
+            body1 = &cbodies[ii]
+            for jj in range(ii + 1, N_BODIES):
+                body2 = &cbodies[jj]
+                dx = body1.x[0] - body2.x[0]
+                dy = body1.x[1] - body2.x[1]
+                dz = body1.x[2] - body2.x[2]
+                ds = dx*dx + dy*dy + dz*dz
+                mag = dt / (ds * sqrt(ds))
+                b1m = body1.m * mag
+                b2m = body2.m * mag
+                body1.v[0] -= dx * b2m
+                body1.v[1] -= dy * b2m
+                body1.v[2] -= dz * b2m
+                body2.v[0] -= dx * b1m
+                body2.v[1] -= dy * b1m
+                body2.v[2] -= dz * b1m
+        for ii in range(N_BODIES):
+            body2 = &cbodies[ii]
+            body2.x[0] += dt * body2.v[0]
+            body2.x[1] += dt * body2.v[1]
+            body2.x[2] += dt * body2.v[2]
+    return make_pybodies(cbodies, N_BODIES)
+
+
+def make c_bodies():
+    pass
+
+
+def make_pybodies():
     pass
