e71740c765e521d4011ed1bd85e5f8f5b6856a04
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-08-22 08:32:09 -0700

    cy/smith/ 5: cython particles++

--- cython/smith/05_extension_types/cython_particle.pyx
+++ cython/smith/05_extension_types/cython_particle.pyx
@@ -11,6 +11,9 @@ cdef class Particle:  # cython extension type
     cpdef get_momentum(self):
         return self.mass * self.velocity
 
+    cdef double get_momentum_c(self):
+        retur self.mass * self.velocity
+
     
 def add_momentums(particles):
     total_mom= 0.
@@ -28,5 +31,36 @@ def add_momemtums_typed(list particles):
     return total_mom
 
 
+def add_momentums_typed_c(list particles):
+    cdef:
+        double totla_mom = 0.
+        Particle particle
+    for particle in particles:
+        total_mom += particle.get_momentum_c()
+    return total_mome
 
 
+cdef class CParticle(Particle):
+    cdef double momentum
+
+    def __init__(self, mass, position, velocity):
+        super().__init__(mass, position, velocity)
+        self.momentum = self.mass * self.velocity
+
+    cpdef double get_momentum(self):
+        return self.momentum
+
+
+class PyParticle(Particle):
+    def __init__(self, mass, position, velocity):
+        super().__init__(mass, position, velocity)
+
+    def get_momentum(self):
+        return super().get_momentum()
+
+
+# calling code
+# p =  Particle() or one of its subclasses
+cdef Particle static_p = p
+print(static_p.get_momentum())
+print(static_p.velocity())
