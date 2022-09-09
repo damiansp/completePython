3df549394b9ea2db2334439a145be2ea1019290a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-09-08 17:01:12 -0700

    cy/sm/6 predefined def files++

--- cython/smith/06_organizing_cython_code/predefined_def_files.pyx
+++ cython/smith/06_organizing_cython_code/predefined_def_files.pyx
@@ -1,9 +1,12 @@
 from libc cimport math
 #from libc.math cimport sin
 from libc.stdlib cimport rand, srand, qsort, malloc, free
-from libc.string cimporm memcopy as c_memcpy
+from libc.string cimport memcopy as c_memcpy
+from libcpp.vector cimport vector
+from math import sin as py_sin  # to avoid namespace collision
 
 
 math.sin(3.14)
 
-cdef int* a = <int*>malloc(10 * sizeof(int))
+cdef int *a = <int*>malloc(10 * sizeof(int))
+cdef vector[int] *vi = new vector[int](10)
