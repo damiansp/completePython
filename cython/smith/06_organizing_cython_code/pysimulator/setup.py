01e8be66779dc48ed200383699d11b68cd67b412
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-09-10 23:10:52 -0700

    cython +pysim setup

---
+++ cython/smith/06_organizing_cython_code/pysimulator/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+from Cython.Build import cythonize
+
+
+setup(
+    name='simulator',
+    packages=[
+        'simulator', 'simulator.core', 'simulator.utils', 'simulator.plugins'],
+    ext_modules=cythonize('**/*.pyx'))
