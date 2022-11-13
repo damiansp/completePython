0383d166755e60a736bcc57397291426c7928123
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-11-12 22:10:04 -0800

    init py spectral norm

---
+++ cython/smith/11_in_practice/py/spectral_norm.py
@@ -0,0 +1,39 @@
+def A(i, j):
+    return 1. / (((i + j) * (i + j + 1) >> 1) + i + 1)
+
+
+def A_times_u(u, v):
+    u_len = len(u)
+    for i in range(u_len):
+        partial_sum = 0.
+        for j in range(u_len):
+            partial_sum += A(i, j) * u[j]
+        v[i] = partial_sum
+
+
+def At_times_u(u, v):
+    u_len = len(u)
+    for i in range(u_len):
+        partial_sum = 0.
+        for j in range(u_len):
+            partial_sum += A(j, i) * u[j]
+        v[i] = partial_sum
+
+
+def B_times_u(u, out, tmp):
+    A_times_u(u, tmp)
+    At_times_u(tmp, out)
+
+
+def spectral_norm(n):
+    u = array('d', [1.] * n)
+    v = array('d', [0.] * n)
+    tmp = array('d', [0.] * n)
+    for _ in range(10):
+        B_times_u(u, v, tmp)
+        B_times_u(v, u, tmp)
+    vBv = vv = 0
+    for ue, ve in zip(u, v):
+        vBv += ue * ve
+        vv += ve * ve
+    return sqrt(vBv / vv)
