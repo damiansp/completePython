1af22627750451c8d51041bf1537cc63290aa618
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-10 22:09:41 -0800

    fin named tuple

--- stdlib/3.10/collections_/named_tuple.py
+++ stdlib/3.10/collections_/named_tuple.py
@@ -21,3 +21,37 @@ Color = namedtuple('Color', 'red green blue')
 Pixel = namedtuple('Pixel', Point._fields + Color._fields)
 px = Pixel(11, 22, 128, 255, 0)
 print(px)
+
+Account = namedtuple('Account', ['type', 'balance'], defaults=[0])
+print(Account._field_defaults)
+print(Account('premium'))
+
+print(getattr(p, 'x'))
+
+d = {'x': 11, 'y': 22}
+print(Point(**d))
+
+
+class Point(namedtuple('Point', ['x', 'y'])):
+    __slots__ = ()
+
+    @property
+    def hypot(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def __str__(self):
+        return f'Point ({self.x:.4f}, {self.y:.4f})'
+
+
+for p in Point(3, 4), Point(14, 5/7):
+    print(f'{p}, {p.hypot:.4f}')
+
+
+Point3D = namedtuple('Point3D', Point._fields + ('z',))
+
+
+Book = namedtuple('Book', ['id', 'title', 'authors'])
+Book.__doc__ += ': Hardcover book in active collection'
+Book.id.__doc__ = '13-digit ISBN'
+Book.title.__doc__ = 'Title of first printing'
+Book.authors.__doc__ = 'List of authors sorted by last name'
