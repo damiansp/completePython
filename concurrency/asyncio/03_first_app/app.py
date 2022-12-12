de151d5daa21a7c36e34fb794137c68f00ee6a80
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-11 21:23:46 -0800

    basi echo server

--- concurrency/asyncio/03_first_app/app.py
+++ concurrency/asyncio/03_first_app/app.py
@@ -6,5 +6,19 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_address = ('127.0.0.1', 8000)
 server_socket.bind(server_address)
 server_socket.listen()
-connection, client_address = server_socket.accept()
-print(f'Connection made with {client_address}')
+
+try:
+    connection, client_address = server_socket.accept()
+    print(f'Connection made with {client_address}')
+    buff = b''
+    while buff[-2:] != '\r\n':
+        data = connection.recv(2)
+        if data:
+            print(f'Got data: {data}!')
+            buff += data
+        else:
+            break
+    print(f'Complete data:\n{buff}')
+    connection.sendall(buff)
+finally:
+    server_socket.close()
