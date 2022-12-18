4dee35bde2ebeccbd675ec44505a37786cf1b37e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-17 17:49:26 -0800

    make connections non-blocking

--- concurrency/asyncio/03_first_app/app.py
+++ concurrency/asyncio/03_first_app/app.py
@@ -6,11 +6,13 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_address = ('127.0.0.1', 8000)
 server_socket.bind(server_address)
 server_socket.listen()
+server_socket.setblocking(False)
 connections = []
 
 try:
     while True:
         connection, client_address = server_socket.accept()
+        connection.setblocking(False)
         print(f'Connection made with {client_address}')
         connections.append(connection)
         for connection in connections:
