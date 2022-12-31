14de4957fee913da01c8ff6df7df8839ec43a9ba
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-31 11:10:13 -0800

    mv collections into data_types; heap_queue++

--- stdlib/3.10/data_types/heap_queue.py
+++ stdlib/3.10/data_types/heap_queue.py
@@ -1,5 +1,8 @@
 # the heap is a binary tree
+from dataclasses import dataclass, field
 import heapq
+import itertools
+from typing import Any
 
 
 x = [29, 100, 98, 5, 13, 59, 17, 73, 40, 16]
@@ -36,3 +39,42 @@ heapq.heappush(h, (3, 'create tests'))
 print(heapq.heappop(h))
 
 
+@dataclass(order=True)
+class PrioritizedItem:
+    priority: int
+    item: Any = field(compare=False)
+
+
+pq = []                      # list of entries arranged in a heap
+entry_finder = {}            # mapping: task -> entry
+REMOVED = '<removed-task>'   # placeholder for removed task
+counter = itertools.count()  # unique seq count
+
+
+def add_task(task, priority=0):
+    'Add a new task or update the priority of an existing task'
+    if task in entry_finder:
+        remove_task(task)
+    count = next(counter)
+    entry = [priority, count, task]
+    entry_finder[task] = entry
+    heappush(pq, entry)
+
+
+def remove_task(task):
+    'Mark an existing task as REMOVED. Raise KeyError if not found'
+    entry = entry_finder.pop(task)
+    entry[-1] = REMOVED
+
+
+def pop_task():
+    'Remove and return the lowest-priority task. Raise KeyError if empty'
+    while pq:
+        priority, count, task = heappop(pq)
+        if task is not REMOVED:
+            del entry_finder[task]
+            return task
+    raise KeyError('pop from an empty priority queue')
+
+
+                             
