791cd54571df9d895b23d3f7661be25dbcb4ef77
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-12-31 11:09:22 -0800

    app2++

--- concurrency/asyncio/03_first_app/app2.py
+++ concurrency/asyncio/03_first_app/app2.py
@@ -1,12 +1,38 @@
 import asyncio
 from asyncio import AbstractEventLoop
 import logging
+import signal
 import socket
+import sys
+from typing import Set
+
+sys.path.append('..')
+from util import delay
 
 
 tasks = []
 
 
+async def main():
+    loop: AbstractEventLoop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, cancel_tasks)
+    await delay(10)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ('127.0.0.1', 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+    await listen_for_conn(server_socket, asyncio.get_event_loop())
+
+
+def cancel_tasks():
+    print('Got SIGINT')
+    tasks: Set[asyncio.Task] = asyncio.all_tasks()
+    print(f'Cancelling {len(tasks)} task(s)')
+    [task.cancel() for task in tasks]
+
+    
 async def echo(conn: socket, loop: AbstractEventLoop) -> None:
     try:
         while data := await loop.sock_recv(conn, 1024):
@@ -25,16 +51,6 @@ async def listen_for_conn(server_socket: socket, loop: AbstractEventLoop):
         conn.setblocking(False)
         print(f'Got connection from {address}')
         tasks.append(asyncio.create_task(echo(conn, loop)))
-
-
-async def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_address = ('127.0.0.1', 8000)
-    server_socket.setblocking(False)
-    server_socket.bind(server_address)
-    server_socket.listen()
-    await listen_for_conn(server_socket, asyncio.get_event_loop())
 
 
 if __name__ == '__main__':
