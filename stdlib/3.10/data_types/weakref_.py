54fb0a898827845c9c4ab427fb6177562b3f22ac
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-02-21 09:26:50 -0800

    weakref++

--- stdlib/3.10/data_types/weakref_.py
+++ stdlib/3.10/data_types/weakref_.py
@@ -1,3 +1,4 @@
+import gc
 import weakref
 
 
@@ -11,7 +12,61 @@ d[k2] = 2  # {k1: 2} [sic], b/c k2 is equal in val to k1
 del k1     # {}
 
 # so do this instead
+k1, k2 = T(), T()
+d = weakref.WeakKeyDictionary()
 d[k1] = 1
 del d[k1]
 d[k2] = 2
 del k1
+
+
+class C:
+    def method(self):
+        print('Method, man!')
+
+
+c = C()
+r = weakref.ref(c.method)
+r()   # 'Method, man!'
+r = weakref.WeakMethod(c.method)
+r()   # <bound method...>
+r()() # 'Method, man!'
+del c
+gc.collect()
+r()   # <None>
+
+
+class Object:
+    pass
+
+
+o = Object()
+r = weakref.ref(o)
+o2 = r()
+print(o is o2)  # True
+del o, o2
+print(r())  # None
+o = r()
+if o is None:
+    print('Object deallocated; cannot frobnicate')
+else:
+    print('Still alive!')
+
+
+class ExtendedRef(weakref.ref):
+    def __init__(self, ob, callback=None, /, **annotations):
+        super().__init__(ob, callback)
+        self.__counter = 0
+        for k, v in annotations.items():
+            setattr(self, k, v)
+
+    def __call__(self):
+        '''Returns a pair containing the referent and the no. of times the
+        reference has been called.
+        '''
+        ob = super().__call__()
+        if ob is not None:
+            self.__counter += 1
+            ob = (ob, self.__counter)
+        return ob
+        
