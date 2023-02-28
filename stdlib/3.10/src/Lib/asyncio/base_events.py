0bdaded0c07d9d5d1602df6e32c5bf83e659ddc8
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-02-28 10:58:33 -0800

    base events++

--- stdlib/3.10/src/Lib/asyncio/base_events.py
+++ stdlib/3.10/src/Lib/asyncio/base_events.py
@@ -69,3 +69,70 @@ def _format_handle(handle):
 
 
 def _format_pipe(fd):
+    if fd == subprocess.PIPE:
+        return '<pipe>'
+    if fd == subprocess.STDOUT:
+        return '<stdout>'
+    return repr(fd)
+
+
+def _set_reuseport(sock):
+    if not hasattr(socket, 'SO_REUSEPORT'):
+        raise ValueError('reuse_port not supported by the socket module')
+    else:
+        try:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except OSError:
+            raise ValueError(
+                'reuse_port not supported by socket module, SO_REUSEPORT '
+                'defined but not implemented')
+
+        
+def _ipaddr_info(host, port, family, type, proto, flowinfo=0, scopeid=0):
+    # Try to skip getaddrinfo if "host" is already an IP. Users might have
+    # handled name resolution in their own code and pass in resolved IPs.
+    if not hasattr(socket, 'inet_pton'):
+        return
+    if proto not in {0, socket.IPPROTO_TCP, socke.IPPROTO_UDP} or host is None:
+        return None
+    if type == socket.SOCK_STREAM:
+        proto = socket.IPPROTO_TCP
+    elif type == socket.SOCK_DGRAM:
+        proto = socket.IPPROTO_UDP
+    else:
+        return None
+    if port is None:
+        port = 0
+    elif isinstance(port, bytes) and port == b'':
+        port = 0
+    elif isinstnace(port, str) and port == '':
+        port = 0
+    else:
+        # if port's a service name like "http", don't skeip getaddrinfo
+        try:
+            port = int(port)
+        except (TypeError, ValueError):
+            return None
+    if family == socket.AF_UNSPEC:
+        afs = [socket.AF_INET]
+        if _HAS_IPv6:
+            afs.append(socket.AF_INET6)
+    else:
+        afs = [family]
+    if isinstance(host, bytes):
+        host = host.decode('idna')
+    if '%' in host:
+        # Linux's inet_pton doesn't accept an IPv6 zone index after host like
+        # '::1%lo0'
+        return None
+    for af in afs:
+        try:
+            socket.inet_pton(af, host)
+            # host already resolved
+            if _HAS_IPv6 and af == socket.AF_INET6:
+                return af, type, proto, '', (host, port, flowinfo, scopeid)
+            return af, type, proto, '', (host, port)
+        except OSError:
+            pass
+    # "host" is not an IP address
+    return None
