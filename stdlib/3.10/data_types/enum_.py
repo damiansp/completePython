8feb9f57dc66c6df74ef3e40ea00b9c215654ac9
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-04-15 11:25:48 -0700

    enum++ ;

--- stdlib/3.10/data_types/enum_.py
+++ stdlib/3.10/data_types/enum_.py
@@ -1,4 +1,5 @@
-from enum import Enum
+from datetime import date
+from enum import auto, Enum# , StrEnum (3.11)
 
 
 class Color(Enum):
@@ -13,3 +14,81 @@ c = Color
 print(c.RED)
 
 print(MyColors.GREEN)
+
+some_var = Color.RED
+print(some_var in Color)
+
+print(Color.GREEN)
+print(Color['BLUE'])
+print(list(Color))
+print(len(Color))
+print(Color.BLUE.name)
+print(Color.BLUE.value)
+
+
+class Weekday(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+    @classmethod
+    def today(cls):
+        print(f'Today is {cls(date.today().isoweekday()).name}')
+
+Weekday.today()
+
+
+class PowersOfThree(Enum):
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return 3 ** (count + 1)
+    FIRST = auto()
+    SECOND = auto()
+    THIRD = auto()
+
+print(PowersOfThree.THIRD.value)
+
+
+'''
+class Build(StrEnum):
+    DEBUG = auto()
+    OPTIMIZED = auto()
+
+    @classmethod
+    def _missing_(cls, value):
+        value = value.lower()
+        for member in cls:
+            if member.value == value:
+                return member
+        return None
+
+print(Build.DEBUG.value)
+print(Build('deBuG'))
+'''
+
+
+class OtherStyle(Enum):
+    ALTERNATE = auto()
+    OTHER = auto()
+    SOMETHING_ELSE = auto()
+
+    def __repr__(self):
+        cls_name = self.__class__.__name__
+        return f'{cls_name}.{self.name}'
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+print(
+    OtherStyle.ALTERNATE, str(OtherStyle.ALTERNATE), f'{OtherStyle.ALTERNATE}')
+print(
+    repr(OtherStyle.ALTERNATE),
+    repr(str(OtherStyle.ALTERNATE)),
+    repr(f'{OtherStyle.ALTERNATE}'))
+
+
