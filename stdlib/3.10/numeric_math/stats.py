b010b967291581728cc1f4576828456c0d582b21
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-04-28 17:38:09 -0700

    stats++

--- stdlib/3.10/numeric_math/stats.py
+++ stdlib/3.10/numeric_math/stats.py
@@ -57,3 +57,42 @@ data = [
     86, 111, 75, 87, 102, 121, 111, 88, 89, 101, 106, 95, 103, 107, 101, 81,
     109, 104]
 print([round(q, 1) for q in st.quantiles(data, n=10)])
+
+
+x = range(1, 10)
+y = [1, 2, 3] * 3
+print(st.covariance(x, y))
+z = range(9, 0, -1)
+print(st.covariance(x, z))
+
+print(st.correlation(x, x))
+print(st.correlation(x, y))
+
+year = [1971, 1975, 1979, 1982, 1983]
+films_cum = [1, 2, 3, 4, 5]
+slope, intercept = st.linear_regression(year, films_cum)
+preds_2023 = round(intercept + slope*2023)
+print(preds_2023)
+
+
+normd = st.NormalDist(mu=35, sigma=7)
+print(normd.mean)
+print(normd.median)
+print(normd.mode)
+print(normd.stdev)
+print(normd.variance)
+
+
+norm_sample = st.NormalDist.from_samples(data)
+print(norm_sample.mean)
+print(norm_sample.median)
+print(norm_sample.mode)
+print(norm_sample.stdev)
+print(norm_sample.variance)
+print(norm_sample.samples(5, seed=1234))
+print(norm_sample.pdf(130))      # prob(130 | norm_sample)
+print(1 - norm_sample.cdf(130))  # prob(>=130 | norm_sample)
+
+print(normd.overlap(norm_sample))
+
+print(normd.quantiles(n=4))
