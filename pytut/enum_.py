d78e86ab1bc7870eb6fb09763f3576592c202574
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-05-03 16:14:40 -0700

    enum++

--- pytut/enum_.py
+++ pytut/enum_.py
@@ -14,3 +14,35 @@ print(Color.RED.value)  # 1
 print(Color.RED in Color)  # True
 print(Color.RED == 1)      # False
 print(Color.RED is 1)      # False
+
+rgb = {
+    Color.RED: '#ff0000',
+    Color.GREEN: '#00ff00',
+    Color.BLUE: '#0000ff'}
+
+print(Color(1))
+prin(Color['RED'] == Color(1))  # True
+
+for color in Color:
+    print(color)
+print(list(Color))
+
+# Errors:
+#Color['YELLOW'] = 3
+#Color.RED.value = 100
+
+
+class Color(Enum):
+    pass
+
+
+class RGB(Color):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class RGBA(RGB):
+    ALPHA = 4
+
+
