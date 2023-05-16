cbeaa58f8453155d7720518e49472d7f4d55786e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-05-16 15:47:18 -0700

    fin enum2

--- pytut/enum2.py
+++ pytut/enum2.py
@@ -15,3 +15,36 @@ print(Color(1))  # RED
 for color in Color:
     print(color)  # RED, GREEN, BLuE
 pprint(Color.__members__)  # shows all
+
+
+class ResponseStatus(Enum):
+    # in progress
+    IN_PROGRESS = 1
+    REQUESTING = 1
+    PENDING = 1
+    # success
+    SUCCESS = 2
+    OK = 2
+    FULFILLED = 2
+    # error
+    ERROR = 3
+    NOT_OK = 3
+    REJECTED = 3
+
+
+code = 'OK'
+if ResponseStatus[code] is ResponseStatus.SUCCESS:
+    print('The request completed successfully')
+
+
+@enum.unique  # ensures no accidental duplicates
+class Day(Enum):
+    MON = 'Monday'
+    TUE = 'Tuesday'
+    WED = 'Wednesday'
+    THU = 'Thursday'
+    FRI = 'Friday'
+    SAT = 'Saturday'
+    SUN = 'Sunday'
+
+
