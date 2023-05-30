ecc98d6f48679fbb6bc3d019e772f05515eaa4f5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-05-30 11:47:56 -0700

    enum_extend +dunders

--- pytut/enum_extend.py
+++ pytut/enum_extend.py
@@ -15,9 +15,40 @@ class PaymentStatus(Enum):
         if isinstance(other, PaymentStatus):
             return self is other
         return False
-    
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.value < other
+        if isinstance(other, PaymentStatus):
+            return self.value < other.value
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, int):
+            return self.value > other
+        if isinstance(other, PaymentStatus):
+            return self.value > other.value
+        return False
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __bool__(self):
+        return self is self.COMPLETED
 
 
 print(PaymentStatus.PENDING)
 if PaymentStatus.PENDING == 1:
     print('Payment pending...')
+
+
+status = 1
+if PaymentStatus.COMPLETED > status:
+    print('The payment has not completed.')
+
+status = PaymentStatus.COMPLETED
+if PaymentStatus.COMPLETED <= status:
+    print('The payment is not pending.')
+    
+for member in PaymentStatus:
+    print(member, bool(member))
