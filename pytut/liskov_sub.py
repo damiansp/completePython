6c346321d67a0ad428f7870c8abcf438de199de2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-13 14:31:23 -0700

    fin liskov

--- pytut/liskov_sub.py
+++ pytut/liskov_sub.py
@@ -37,9 +37,46 @@ class NotificationManager:
         else:
             raise Exception('The notification is not supported')
 
+
+# Better implementation (conform to Liskov Substitution principle)
+class Notification(ABC):
+    @abstractmethod
+    def notify(self, msg):
+        pass
+
+
+class Email(Notification):
+    def __init__(self, email):
+        self.email = email
+
+    def notify(self, msg):
+        print(f'Emailing "{msg}" to {self.email}')
+
+
+class SMS(Notification):
+    def __init__(self, phone):
+        self.phone = phone
+
+    def notify(self, msg):
+        print(f'Texting "{msg}" to {self.phone}')
+
+
+class NotificationManager:
+    def __init__(self, notification):
+        self.notification = notification
+
+    def send(self, msg):
+        self.notification.notify(msg)
+
         
 if __name__ == '__main__':
     contact = Contact(
         'John Jacob', 'jj@jingleheimer-schmidt.com', '(213) 456-7890')
-    notification_mgr = NotificationManager(SMS(), contact)
-    notification_mgr.send('Hello, JJ')
+    #notification_mgr = NotificationManager(SMS(), contact)
+    #notification_mgr.send('Hello, JJ')
+    sms_notification = SMS(contact.phone)
+    email_notification = Email(contact.email)
+    notification_mgr = NotificationManager(sms_notification)
+    notification_mgr.send('Hello, John')
+    notification_mgr.notification = email_notification
+    notification_mgr.send("We're watching you")
