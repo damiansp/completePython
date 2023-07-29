bd56aa78bfefc6ad76be51a6f96bba4b93423bf0
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-29 14:02:48 -0700

    init dep inv

---
+++ pytut/dependency_inversion_principle.py
@@ -0,0 +1,49 @@
+from abc import ABC
+
+
+class FXConverter1:
+    def convert(self, from_currency, to_currency, amt):
+        print(f'{amt} {from_currency} -> {1.2 * amt} {to_currency}')
+        return 1.2 * amt
+
+class App1:
+    def start(self):
+        converter = FXConverter1()
+        converter.convert('EUR', 'USD', 100)
+
+
+# Better implementation
+class CurrencyConverter(ABC):
+    def convert(self, from_currency, to_currency, amt) -> float:
+        pass
+
+
+class FXConverter(CurrencyConverter):
+    def convert(self, from_currency, to_currency, amt) -> float:
+        print('Converting currency using FX API')
+        print(f'{amt} {from_currency} -> {1.2 * amt} {to_currency}')
+        return 1.2 * amt
+
+
+class App:
+    def __init__(self, converter: CurrencyConverter):
+        self.converter = converter
+
+    def start(self):
+        self.converter.convert('EUR', 'USD', 100)
+
+
+# in the future...
+
+def main():
+    app1 = App1()
+    app1.start()
+
+    converter = FXConverter()
+    app = App(converter)
+    app.start()
+    
+    
+
+if __name__ == '__main__':
+    main()
