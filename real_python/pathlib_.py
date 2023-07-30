e92854efd8d2190a644783cdea34254c0e267af3
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-07-29 21:35:23 -0700

    pathlib++

--- real_python/pathlib_.py
+++ real_python/pathlib_.py
@@ -13,3 +13,18 @@ THIS_FILE = Path(__file__)
 PARENT_DIR = THIS_FILE.parent
 print(THIS_FILE)
 print(PARENT_DIR)
+
+# Joining paths
+for filepath in Path.cwd().glob('*.txt'):
+    new_path = Path('archive') / filepath.name
+    filepath.rename(new_path)
+
+
+# File system ops with paths
+path = Path('~/Learning/python/real_python/test.md')
+print(path.name)    # test.md
+print(path.stem)    # test
+print(path.suffix)  # .md
+print(path.anchor)  # (None; Root drive like C:\\ in windows)
+print(path.parent)  # ~/Learning/python/real_python
+
