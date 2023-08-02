a565de5cadc6c25053027d25c576f74a0d6bb011
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-08-02 09:46:08 -0700

    +shopping list; pathlib++

--- real_python/pathlib_.py
+++ real_python/pathlib_.py
@@ -28,3 +28,38 @@ print(path.suffix)  # .md
 print(path.anchor)  # (None; Root drive like C:\\ in windows)
 print(path.parent)  # ~/Learning/python/real_python
 
+
+# Reading / Writing
+# read shopping_list.md
+path = Path.cwd() / 'shopping_list.md'
+with path.open(mode='r', encoding='utf-8') as f:
+    content = f.read()
+    groceries = [line for line in content.splitlines() if line.startswith('*')]
+print('\n'.join(groceries))
+
+
+# alternately
+content = path.read_text(encoding='utf-8')
+groceries = [line for line in content.splitlines() if line.startswith('*')]
+print('\n'.join(groceries))
+
+# or even
+content = Path('shopping_list.md').read_text(encoding='utf-8')  # cwd assumed
+groceries = [line for line in content.splitlines() if line.startswith('*')]
+
+# write
+Path('plain_list.md').write_text('\n'.join(groceries), encoding='utf-8')
+
+
+# Renaming files
+txt_path = Path('hello.txt')
+md_path = txt_path.with_suffix('.md')
+print(md_path)
+#txt_path.replace(md_path)  # writes ./hello.md
+
+md_path = txt_path.with_name('greeting.md')
+print(md_path)
+#txt_path.replace(md_path)  # writes ./greeting.md
+
+
+# Copying files
