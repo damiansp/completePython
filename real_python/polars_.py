478bdcdb4b8cafa7729447b694d5e92263acbd1f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-03 15:10:09 -0700

    polars++

--- real_python/polars_.py
+++ real_python/polars_.py
@@ -4,8 +4,9 @@ import polars as pl
 
 def main():
     buildings = init_buildings_data()
-
-
+    show_sample_selects(buildings)
+    filter_example(buildings)
+    
 
 def init_buildings_data():
     N = 5_000
@@ -19,6 +20,18 @@ def init_buildings_data():
     print(data.describe())
     return data
 
+
+def show_sample_selects(buildings):
+    print(buildings.select('sqft'))
+    print(buildings.select(pl.col('sqft')))
+    print(buildings.select(pl.col('sqft').sort() / 1000))
+
+
+def filter_example(buildings):
+    after_2015 = buildings.filter(pl.col('year') > 2015)
+    print(after_2015.shape)
+    print(after_2015.select(pl.col('year').min()))
+    
 
 if __name__ == '__main__':
     main()
