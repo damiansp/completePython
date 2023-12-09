8275bd520c8b7c43598f95ffe42fad088ac406bd
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-08 20:43:23 -0800

    init meta example

---
+++ pytut/metaclass_example.py
@@ -0,0 +1,72 @@
+from pprint import pprint
+
+
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, val):
+        self._name = val
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, val):
+        self._age = val
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __hash__(self):
+        return hash(f'{self.name}, {self.age}')
+
+    def __str__(self):
+        return f'Person(name={self.name}, age={self.age})'
+
+    def __repr__(self):
+        return f'Person(name={self.name}, age={self.age})'
+
+
+class Prop:
+    def __init__(self, attr):
+        self._attr = attr
+
+    def get(self, obj):
+        return getattr(obj, self._attr)
+
+    def set(self, obj, val):
+        return setattr(obj, self._attr, val)
+
+    def delete(self, obj):
+        return delattr(obj, self._attr)
+
+
+class Data(type):
+    def __new__(mcs, name, bases, class_dict):
+        class_obj = super().__new__(mcs, name, bases, class_dict)
+        Data.define_property(class_obj)
+        return class_obj
+
+    @staticmethod
+    def define_property(class_obj):
+        for prop in class_obj.props:
+            attr = f'_{prop}'
+            prop_obj = property(
+                fget=Prop(attr).get, fset=Prop(attr).set, fdel=Prop(attr).delete)
+            setattr(class_obj, prop, prop_obj)
+        return class_obj
+
+
+class Person(metaclass=Data):
+    props = ['name', 'age']
+
+
+pprint(Person.__dict__)
