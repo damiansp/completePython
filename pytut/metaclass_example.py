07e9981c7073fec77c3d0f488e39c3895643fd3a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-18 09:39:31 -0800

    +cicd +metaclass ex

--- pytut/metaclass_example.py
+++ pytut/metaclass_example.py
@@ -54,13 +54,31 @@ class Data(type):
         class_obj = super().__new__(mcs, name, bases, class_dict)
         Data.define_property(class_obj)
         return class_obj
+        # define __init__
+        setattr(class_obj, '__init__', Data.init(class_obj))
+        return class_obj
+
+    @staticmethod
+    def init(class_obj):
+        def _init(self, *obj_args, **obj_kwargs):
+            if obj_kwargs:
+                for prop in class_obj.props:
+                    if prop in obj_kwargs.keys():
+                        setattr(self, prop, obj_kwargs[prop])
+            if obj_args:
+                for kv in zip(class_obj.props, obj_args):
+                    setattr(self, kv[0], kv[1])
+
+        return _init
 
     @staticmethod
     def define_property(class_obj):
         for prop in class_obj.props:
             attr = f'_{prop}'
             prop_obj = property(
-                fget=Prop(attr).get, fset=Prop(attr).set, fdel=Prop(attr).delete)
+                fget=Prop(attr).get,
+                fset=Prop(attr).set,
+                fdel=Prop(attr).delete)
             setattr(class_obj, prop, prop_obj)
         return class_obj
 
@@ -70,3 +88,5 @@ class Person(metaclass=Data):
 
 
 pprint(Person.__dict__)
+p = Person('John Doe', age=25)
+print(p.__dict__)
