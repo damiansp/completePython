b3a70074deb13dbff5c3d26fc16116bec16edc49
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-03-02 11:01:38 -0800

    init raise from; +tokenize script

---
+++ internals/05_language_and_grammar/tokenize.sh
@@ -0,0 +1,1 @@
+../cpython/python.exe -m tokenize -e test_tokens.py
---
+++ pytut/raise_from.py
@@ -0,0 +1,24 @@
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError('b cannot be 0')
+
+    
+def divide2(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        raise ValueError('b cannot be 0') from e
+
+
+
+#divide(3, 0)
+#divide2(4, 0)
+
+
+try:
+    divide2(4, 0)
+except ValueError as e:
+    print('Cause:', e.__cause__)
+    print('Exception:', e)
