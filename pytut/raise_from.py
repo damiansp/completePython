5b695d1c15895392b8eb529bcac87d0de36f020d
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-03-10 15:53:32 -0700

    fin raise from

--- pytut/raise_from.py
+++ pytut/raise_from.py
@@ -22,3 +22,20 @@ try:
 except ValueError as e:
     print('Cause:', e.__cause__)
     print('Exception:', e)
+
+
+def divide3(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        raise ValueError('b cannot be 0') from None
+
+
+try:
+    divide3(10, 0)
+except ValueError as e:
+    print('Cause:', e.__cause__)
+    print('Exception:', e)
+
+
+
