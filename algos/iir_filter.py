edd254a88cc6203d624ed007a86357d74d6c2f7c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-04-30 12:18:18 -0700

    fin iir filt; +yt downloader

--- algos/iir_filter.py
+++ algos/iir_filter.py
@@ -29,4 +29,37 @@ class IIRFilter:
             >>> filt = IIRFilter(2)
             >>> filt.set_coefficients(a_coeffs, b_coeffs)
         '''
-        pass # TODO
+        if len(a_coefs) < self.order:
+            a_coefs = [1., *a_coefs]
+        if len(a_coefs) != self.order + 1:
+            msg = (
+                f'Expected a_coefs to have {self.order + 1} elements for '
+                f'{self.order}-order filter; got {len(a_coefs)}')
+            raise ValueError(msg)
+        if len(b_coefs) != self.order + 1:
+            msg = (
+                f'Expected b_coefs to have {self.order + 1} elements for '
+                f'{self.order}-order filter; got {len(b_coefs)}')
+            raise ValueError(msg)
+        self.a_coefs = a_coefs
+        self.b_coefs = b_coefs
+
+    def process(self, sample: float) -> float:
+        '''Calculate y[n]
+        >>> filt = IIRFilter(2)
+        >>> filt.process(0)
+        0.0
+        '''
+        res = 0.
+        # Start at index 1 and do index 0 at end
+        for i in range(1, self.order + 1):
+            res += (
+                self.b_coefs[i] * self.input_history[i - 1]
+                - self.a_coefs[i] * self.output_history[i - 1])
+        res = (res + self.b_coefs[0]*sample) / self.a_coefs[0]
+        self.input_history[1:] = self.input_history[:-1]
+        self.ouput_history[1:] = self.output_history[:-1]
+        self.input_history[0] = sample
+        self.output_history[0] = res
+        return res
+        
