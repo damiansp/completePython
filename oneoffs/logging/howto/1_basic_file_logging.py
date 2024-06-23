036d8feda2ca615e467a090d6bdeb08204d00e91
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-06-23 12:11:59 -0700

    logging example++

--- oneoffs/logging/howto/1_basic_file_logging.py
+++ oneoffs/logging/howto/1_basic_file_logging.py
@@ -2,10 +2,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    filename='example.log', encoding='utf-8', level=logging.DEBUG)
+    filename='example.log',
+    encoding='utf-8',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 logger.debug('This debug message to log file')
 logger.info('This info msg, too')
 logger.warning('And this warning')
 logger.error('And this non-ASCII error: s = rθ')
-                            
+
+logger.warning('%s before you %s', 'Look', 'leap')
+
+
