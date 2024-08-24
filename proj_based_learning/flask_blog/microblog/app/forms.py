dddaccd4a98cad98f8239420c34e48c4bc899482
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-24 00:22:43 -0700

    flesh out login

--- proj_based_learning/flask_blog/microblog/app/forms.py
+++ proj_based_learning/flask_blog/microblog/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wrforms.validators import DataRequired
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
--- proj_based_learning/flask_blog/microblog/app/routes.py
+++ proj_based_learning/flask_blog/microblog/app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import flash, redirect, render_template
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -13,3 +14,14 @@ def index():
         {'author': {'username': 'Susan'},
          'body': 'The Avengers movie was so cool!'}]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested fro user {form.username.data}, '
+            f'remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
