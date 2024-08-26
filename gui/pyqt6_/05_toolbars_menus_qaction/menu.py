3955a44c5813ace19bd0f5d52289d1a2c1d1d486
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-26 14:58:24 -0700

    menu++

--- gui/pyqt6_/05_toolbars_menus_qaction/menu.py
+++ gui/pyqt6_/05_toolbars_menus_qaction/menu.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtGui import QAction, QIcon, QKeySequence
 from PyQt6.QtWidgets import (
     QApplication, QCheckBox, QLabel, QMainWindow, QStatusBar, QToolBar)
 
@@ -20,9 +20,10 @@ class MainWindow(QMainWindow):
         label = self._add_label()
         self.setCentralWidget(label)
         self._add_toolbar()
-        self.button = None  # made in _add_toolbar
+        self.button1 = None  # made in _add_toolbar
+        self.button2 = None  # made in _add_toolbar
         self.setStatusBar(QStatusBar(self))
-        menu = self._add_menu()
+        self._add_menu()
         
     def _add_label(self):
         lab = QLabel('Howdy!')
@@ -34,11 +35,13 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
         button = self._make_toolbar_button('&Your button', 'I am your button')
-        self.button = button
+        self.button1 = button
+        self.button1.setShortcut(QKeySequence('Ctrl+p'))
         toolbar.addAction(button)
         toolbar.addSeparator()
         button2 = self._make_toolbar_button(
             'Your &button2', 'I am your button, too')
+        self.button2 = button2
         toolbar.addAction(button2)
         toolbar.addWidget(QLabel('Hi there!'))
         toolbar.addWidget(QCheckBox())
@@ -56,8 +59,10 @@ class MainWindow(QMainWindow):
     def _add_menu(self):
         menu = self.menuBar()
         file_menu = menu.addMenu('&File')
-        file_menu.addAction(self.button)
-        return menu
+        file_menu.addAction(self.button1)
+        file_menu.addSeparator()
+        file_submenu = file_menu.addMenu('Submenu')
+        file_submenu.addAction(self.button2)
         
 
 if __name__ == '__main__':
