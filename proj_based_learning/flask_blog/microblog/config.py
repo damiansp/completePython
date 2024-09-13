1de1f680cede4ef2d68565381694f3efabf2ea37
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-09-12 21:14:13 -0700

    add db to config

--- proj_based_learning/flask_blog/microblog/config.py
+++ proj_based_learning/flask_blog/microblog/config.py
@@ -1,5 +1,10 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'fake-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app.db"}')
