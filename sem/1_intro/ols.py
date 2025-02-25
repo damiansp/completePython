eaab69143c9f3dad4f397efba54e93f8e3a136a3
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-02-24 22:36:33 -0800

    ols +t and example

--- sem/1_intro/ols.py
+++ sem/1_intro/ols.py
@@ -35,3 +35,30 @@ def check_assumptions(X, epsilon):
     normality = np.random.normal()  # placeholder for actual test res
     return homosked, no_multicollinearity, normality
     
+
+def calculate_t_statistic(beta_hat, X, sigma_hat_square, j):
+    '''Calculate t stat for given coef.
+    Parameters:
+    - beta_hat: estimated coefs
+    - X: model matrix
+    - sigma_hat_square: estimated variance of errors
+    - j: indix for beta coef
+    '''
+    cov_mat = sigma_hat_square * inv(X.T @ X)
+    se_beta_j = np.sqrt(cov_mat[j, j])
+    t = beta_hat[j] / se_beta_j
+    return t
+
+
+if __name__ == '__main__':
+    X = np.array([[1, 2], [1, 3], [1, 4]])  # w intercept
+    y = np.array([1, 2, 3])
+    eps = np.random.normal(0, 1, len(y))
+    beta_hat = ols_estimator(X, y)
+    assumptions = check_assumptions(X, eps)
+    sig_hat_sq = np.var(eps)
+    j = 1
+    t_stat = calculate_t_statistic(beta_hat, X, sig_hat_sq, j)
+    print('Est Coefs:', beta_hat)
+    print('Assumptions: (Homosked., No mulit-collin., Nomality):', assumptions)
+    print(f't for Beta[{j}]: {t_stat}')
