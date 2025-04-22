60f59d23587cc13f634868eb4cf6f35b1115213c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-04-21 21:49:38 -0700

    +1.13

--- cookbook/1_data_structures_algos/13_sort_list_of_dicts.py
+++ cookbook/1_data_structures_algos/13_sort_list_of_dicts.py
@@ -1,1 +1,20 @@
-#
+from operator import itemgetter
+
+
+rows = [
+    {'name': 'Brian', 'sur': 'Jones', 'id': 1003},
+    {'name': 'David', 'sur': 'Beazley', 'id': 1002},
+    {'name': 'John', 'sur': 'Cleese', 'id': 1001},
+    {'name': 'Eric', 'sur': 'Idle', 'id': 1004},
+    {'name': 'James Earl', 'sur': 'Jones', 'id': 1000}]
+rows_by_sur = sorted(rows, key=itemgetter('sur'))
+rows_by_id = sorted(rows, key=itemgetter('id'))
+print(rows_by_sur)
+print(rows_by_id)
+rows_by_sn = sorted(rows, key=itemgetter('sur', 'name'))
+# equivalently:
+# rows_by_sn = sorted(rows, key=lambda: r: (r['sur'], r['name']))
+print(rows_by_sn)
+
+
+print(max(rows, key=itemgetter('id')))
