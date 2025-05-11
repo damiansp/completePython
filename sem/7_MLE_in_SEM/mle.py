bcb3b5b1706fd25957da64b0aa366b2581a61eb0
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-05-11 14:14:23 -0700

    MLE++

--- sem/7_MLE_in_SEM/mle.py
+++ sem/7_MLE_in_SEM/mle.py
@@ -18,3 +18,46 @@ def log_likelihood(y, mu, sigma_theta):
         - 0.5 * np.sum(diff @ inv_sig * diff, axis=1).sum())
 
 
+def em_algorithm(y, mu_init, sigma_init, tol=1e-6, max_iter=1000):
+    '''Expectation Maximization
+    Parameters:
+    - y: observed data
+    - mu_init: initial mean estimate
+    - signa_init: initial sigma estimate
+    Returns: estimated mean and cov matrix
+    '''
+    mu = mu_init
+    sig = sigma_init
+    n_iter = 0
+    while n_iter < max_iter:
+        mu_new = np.mean(y, axis=0)
+        sig_new = np.cov(y, rowvar=False)
+        if (np.allclose(mu, mu_new, atol=tol)
+                and np.allclose(sig, sig_new, atol=tol)):
+            break
+        mu = mu_new
+        sig = sig_new
+        n_ter += 1
+    return mu, sig
+
+
+def compute_hessian(y, mu, sigma_theta):
+    '''Compute Hessian of log-likelihood function
+    Parameters:
+    - y: observed data
+    - mu: mean estimates
+    - sigma_theta: cov matrix est
+    Returns: Hessian
+    '''
+    inv_sig = np.linalg.inv(sigma_theta)
+    diff = y - mu
+    n, p = y.shape
+    # simplistic appx of hessian
+    return (
+        n * np.kron(inv_sig, inv_sig)
+        - np.einsum('i,ij,ik->jk', np.ones(n), diff, diff)
+        @ np.kron(inv_sig, inv_sig))
+
+
+if __name__ == '__main__':
+    pass
