aa46800e495f1d101cc1fd4afb9d14b6431eae41
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2025-07-19 00:38:56 -0700

    chainmap++

--- cookbook/1_data_structures_algos/20_chainmap.py
+++ cookbook/1_data_structures_algos/20_chainmap.py
@@ -7,3 +7,15 @@ c = ChainMap(a, b)
 print(c['x'])  # 1 from a
 print(c['y'])  # 2 from b
 print(c['z'])  # 3 from a
+
+
+values = ChainMap()
+values['x'] = 1
+values = values.new_child()
+values['x'] = 2
+values = values.new_child()
+values['x'] = 3
+print(values)
+print(values['x'])  # 3
+values = values.parents
+print(values['x'])  # 2
