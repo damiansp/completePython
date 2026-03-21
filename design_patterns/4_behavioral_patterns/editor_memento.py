6989101c1272e824e11af69e138e0b9d27215862
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2026-03-20 22:22:30 -0700

    fin editor memento

--- design_patterns/4_behavioral_patterns/editor_memento.py
+++ design_patterns/4_behavioral_patterns/editor_memento.py
@@ -1,3 +1,18 @@
+def main():
+    ed = Editor()
+    hist = History()
+    ed.type('This is the first sentence.')
+    hist.push(ed.save())
+    ed.type(' And this is the second.')
+    hist.push(ed.save())
+    ed.type('  And now a third.')
+    print(ed.get_content())
+    ed.restore(hist.pop())
+    print(ed.get_content())
+    ed.restore(hist.pop())
+    print(ed.get_content())
+
+
 class EditorMemento:
     def __init__(self, content):
         self._content = content
@@ -7,4 +22,32 @@ class EditorMemento:
 
 
 class Editor:
-    pass
+    def __init__(self):
+        self._content = ''
+
+    def type(self, words):
+        self._content += words
+
+    def get_content(self):
+        return self._content
+
+    def save(self):
+        return EditorMemento(self._content)
+
+    def restore(self, memento):
+        self._content = memento.get_content()
+
+
+class History:
+    def __init__(self):
+        self._mementos = []
+
+    def push(self, memento):
+        self._mementos.append(memento)
+
+    def pop(self):
+        return self._mementos.pop() if self._mementos else None
+
+
+if __name__ == '__main__':
+    main()
